6c08a8de5a9a5a26525e6029b1b5c702b50e354b
onongol <rozserbatlin@gmail.com>
2025-08-13 14:26:38 +0800

    Remove GitHub Actions conditional database configuration and clean up settings

--- depo_crud/settings.py
+++ depo_crud/settings.py
@@ -108,13 +108,6 @@ DATABASES = {
     }
 }
 
-if os.getenv("GITHUB_ACTIONS") == "true":
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
