5752c9efa88f7cfdfa41814bf66f317cbab9b7d6
onongol <rozserbatlin@gmail.com>
2025-09-09 13:24:34 +0800

    Add docstring to CustomUserCreationForm for clarity on employee_id field

--- employee/forms/register_forms.py
+++ employee/forms/register_forms.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
+    """
+    User registration form with additional employee_id field.
+    """
     employee_id = forms.CharField(
         label="Employee ID",
         required=True,
