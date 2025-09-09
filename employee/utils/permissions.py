3f88f3526995d56405b9a2910b8169e2c4212579
onongol <rozserbatlin@gmail.com>
2025-09-09 16:06:46 +0800

    Refactor permission checks to use GroupNames enum for consistency and clarity

--- employee/utils/permissions.py
+++ employee/utils/permissions.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
 
+from employee.constants.constants import GroupNames
 
 class OnlyGroupMixin(UserPassesTestMixin):
     """Mixin to restrict access to users in specific groups or superusers."""
@@ -11,16 +12,16 @@ class OnlyGroupMixin(UserPassesTestMixin):
 
 
 def is_admin(user):
-    return user.is_superuser or user.groups.filter(name__in=['Payrolls']).exists()
+    return user.is_superuser or user.groups.filter(name__in=[GroupNames.PAYROLLS.value]).exists()
 
 
 class OnlyAdminMixin(OnlyGroupMixin):
-    group_names = ['Payrolls']
+    group_names = [GroupNames.PAYROLLS.value]
 
 
 def is_creater(user):
-    return user.is_superuser or user.groups.filter(name__in=['Payrolls', 'Masters']).exists()
+    return user.is_superuser or user.groups.filter(name__in=[GroupNames.PAYROLLS.value, GroupNames.MASTERS.value]).exists()
 
 
 class OnlyCreaterMixin(OnlyGroupMixin):
-    group_names = ['Payrolls', 'Masters']
+    group_names = [GroupNames.PAYROLLS.value, GroupNames.MASTERS.value]
