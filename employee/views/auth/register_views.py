b00775a89925f7c178087a774a749e1fe162a906
onongol <rozserbatlin@gmail.com>
2025-09-10 09:55:41 +0800

    Refactor register_view to streamline instance retrieval and improve error handling

--- employee/views/auth/register_views.py
+++ employee/views/auth/register_views.py
@@ -4,8 +4,8 @@ from django.contrib import messages
 
 from employee.forms.register_forms import CustomUserCreationForm
 from employee.models import Employee
-from employee.models.master_models import Master  
-from employee.models.payroll_models import Payroll
+from employee.models import Master  
+from employee.models import Payroll
 from employee.constants.constants import GroupNames
 
 
@@ -17,31 +17,40 @@ def link_user_to_instance(user, instance, group_name):
     instance.save()
 
 
+def find_instance_by_id(register_id):
+    """Find the instance and corresponding group by the given register ID."""
+    for model, id_field, group_name in [
+        (Employee, 'employee_id', GroupNames.EMPLOYEES.value),
+        (Master, 'master_id', GroupNames.MASTERS.value),
+        (Payroll, 'payroll_id', GroupNames.PAYROLLS.value),
+    ]:
+        # Find the instance by ID
+        instance = model.objects.filter(**{id_field: register_id}).first()
+        if instance:
+            return instance, group_name
+    return None, None
+
+
 def register_view(request):
     """User registration view."""
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             register_id = form.cleaned_data.get('employee_id')
-            user = None
-            for model, id_field, group_name, not_found_msg in [
-                (Employee, 'employee_id', GroupNames.EMPLOYEES.value, ("Employee not found.")),
-                (Master, 'master_id', GroupNames.MASTERS.value, ("Master not found.")),
-                (Payroll, 'payroll_id', GroupNames.PAYROLLS.value, ("Payroll not found.")),
-            ]:
-                try:
-                    instance = model.objects.get(**{id_field: register_id})
-                    if instance.user:
-                        form.add_error('employee_id', ("A user is already linked to this ID."))
-                        return render(request, 'auth/register.html', {'form': form})
+            # Try to find the corresponding instance (Employee, Master, or Payroll) by ID
+            instance, group_name = find_instance_by_id(register_id)
+            if instance:
+                # Check if this instance is already linked to a user
+                if instance.user:
+                    form.add_error('employee_id', "A user is already linked to this ID.")
+                else:
                     user = form.save()
                     link_user_to_instance(user, instance, group_name)
-                    messages.success(request, ("Registration successful! Please log in with your new account."))
+                    messages.success(request, "Registration successful! Please log in with your new account.")
                     return redirect('login')
-                except model.DoesNotExist:
-                    continue
-            form.add_error('employee_id', ("Employee, Master, or Payroll not found."))
-            return render(request, 'auth/register.html', {'form': form})
+            else:
+                form.add_error('employee_id', "Employee, Master, or Payroll not found.")
+        return render(request, 'auth/register.html', {'form': form})
     else:
         form = CustomUserCreationForm()
     return render(request, 'auth/register.html', {'form': form})
