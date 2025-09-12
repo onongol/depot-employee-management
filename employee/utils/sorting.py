c036f4b60ae11aee9c7652dcf25a1cdac543a4c2
onongol <rozserbatlin@gmail.com>
2025-09-12 15:22:56 +0800

    Add apply_ordering function for flexible queryset sorting

---
+++ employee/utils/sorting.py
@@ -0,0 +1,14 @@
+def apply_ordering(queryset, order_by, direction, allowed_fields, default='-record_date'):
+    """
+    Applies ordering to a queryset based on the specified field and direction.
+    :param queryset: QuerySet to be ordered
+    :param order_by: field name to order by (str)
+    :param direction: 'asc' or 'desc'
+    :param allowed_fields: list of allowed fields for ordering
+    :param default: default ordering field
+    :return: ordered QuerySet
+    """
+    if order_by in allowed_fields:
+        sort_field = f"-{order_by}" if direction == 'desc' else order_by
+        return queryset.order_by(sort_field)
+    return queryset.order_by(default)
