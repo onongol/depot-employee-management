ccb3796819a82a1cd0affcd8c7e6809ea828fe73
onongol <rozserbatlin@gmail.com>
2025-09-25 15:31:14 +0800

    Add translation support for delete warning message in send_delete_warning function

--- employee/mixins/delete_attention.py
+++ employee/mixins/delete_attention.py
@@ -1,9 +1,12 @@
 from django.contrib import messages
+from django.utils.translation import gettext_lazy as _ 
 
 
 def send_delete_warning(request, object_name):
     """Send a warning message after deleting an object."""
     messages.warning(
         request,
-        f'You have deleted {object_name}. Also, all related records have been deleted.'
+        _('You have deleted %(object_name)s. Also, all related records have been deleted.') % {
+            'object_name': object_name
+        }
     )
