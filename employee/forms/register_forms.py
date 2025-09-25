7c80fed247703a15dfb3e638cb43c1ed36268ab2
onongol <rozserbatlin@gmail.com>
2025-09-25 13:26:29 +0800

    Add translation support for Employee ID label and help text in registration form

--- employee/forms/register_forms.py
+++ employee/forms/register_forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -8,9 +9,9 @@ class CustomUserCreationForm(UserCreationForm):
     User registration form with additional employee_id field.
     """
     employee_id = forms.CharField(
-        label="Employee ID",
+        label=_("Employee ID"),
         required=True,
-        help_text="Enter your Employee ID to link your account."
+        help_text=_("Enter your Employee ID to link your account.")
     )
 
     class Meta:
