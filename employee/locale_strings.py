ba9578adc32c37edcd74eb3d8efd0913d61cf8cc
onongol <rozserbatlin@gmail.com>
2025-09-25 13:25:24 +0800

    Add password validation messages for localization support

---
+++ employee/locale_strings.py
@@ -0,0 +1,5 @@
+from django.utils.translation import gettext_noop
+
+gettext_noop("Your password can’t be too similar to your other personal information.")
+gettext_noop("Your password can’t be a commonly used password.")
+gettext_noop("Your password can’t be entirely numeric.")
