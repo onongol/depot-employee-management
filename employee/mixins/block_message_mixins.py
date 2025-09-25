6a3c73bcc5239e640ba181a2f174285373b37643
onongol <rozserbatlin@gmail.com>
2025-09-25 15:54:25 +0800

    Add BlockMessageMixin to provide generic block message handling with translation support

---
+++ employee/mixins/block_message_mixins.py
@@ -0,0 +1,20 @@
+from django.utils.translation import gettext_lazy as _
+
+class BlockMessageMixin:
+    """
+    Provides a generic get_block_message based on a list of related record labels.
+    Views can set block_related_models = ['Daily Salary', 'Piecework'].
+    """
+    block_related_models: list[str] = []
+
+    def get_block_message(self):
+        if not self.block_related_models:
+            return ''
+        related = ', '.join(str(_(label)) for label in self.block_related_models)
+        return _(
+            "Cannot delete %(object_name)s because it is associated with %(related)s records. "
+            "Please remove the related %(related)s records first."
+        ) % {
+            'object_name': self.get_object_name(),
+            'related': related
+        }
