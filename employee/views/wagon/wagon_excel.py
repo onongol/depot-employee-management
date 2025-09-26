fa9bbc6bb5e4ac349713bbe8ffad469a38bf1af5
onongol <rozserbatlin@gmail.com>
2025-09-26 10:31:40 +0800

    Refactor export_wagon_excel to include department in filter and use translated headers for Excel export

--- employee/views/wagon/wagon_excel.py
+++ employee/views/wagon/wagon_excel.py
@@ -1,3 +1,5 @@
+from django.utils.translation import gettext_lazy as _
+
 from employee.utils.export_excel import export_to_excel
 from .wagon_filtered import wagon_filter
 from .wagon_grouping import get_grouped_wagon_data, regroup_and_sum_wagon_data
@@ -5,15 +7,27 @@ from .wagon_grouping import get_grouped_wagon_data, regroup_and_sum_wagon_data
 
 def export_wagon_excel(request):
     """Export wagon data to Excel."""
-    pieceworks, wagon_number, work_name, work_date = wagon_filter(request)
+    pieceworks, wagon_number, work_name, work_date, department = wagon_filter(request)
 
     wagon_data = get_grouped_wagon_data(pieceworks)
 
     # Group and sum by wagon_number, work__work_name, work_date. Convert grouped dict to list for export
     grouped_wagon_data, total_amount, total_time, total_price = regroup_and_sum_wagon_data(wagon_data)
 
+    headers = [
+        _("Wagon Number"),
+        _("Type Work"),
+        _("Work Name"),
+        _("Amount"),
+        _("Total Time"),
+        _("Total Price"),
+        _("Date"),
+    ]
+
+    headers = [str(h) for h in headers]
+
     # Format data for export to Excel
-    export_data = [
+    data = [
         [
             row['wagon_number'],
             row['type_work'],
@@ -26,11 +40,15 @@ def export_wagon_excel(request):
         for row in grouped_wagon_data
     ]
 
-    headers = [
-        "Wagon Number", "Type Work", "Work Name", "Amount", "Total Time", "Total Price", "Date",
-    ]
+    total_str = _("Total")
+    total_str = str(total_str)
 
-    # Add totals row
-    export_data.append(["Total", "", "", total_amount, total_time, total_price, ""])
+    # Append totals row
+    data.append([total_str, "", "", total_amount, total_time, total_price, ""])
 
-    return export_to_excel(export_data, headers, "wagon.xlsx", "Wagon")
+    file_name = "wagon.xlsx"
+
+    sheet_title = _("Wagon")
+    sheet_title = str(sheet_title)
+
+    return export_to_excel(data, headers, file_name, sheet_title)
