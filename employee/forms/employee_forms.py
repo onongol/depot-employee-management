c8bcc2c0cf987547ddd4f0b6e71838157cbeb1de
onongol <rozserbatlin@gmail.com>
2025-10-06 11:06:02 +0800

    Add money_per_hour field to EmployeeForm and UpdateEmployeeForm

--- employee/forms/employee_forms.py
+++ employee/forms/employee_forms.py
@@ -14,7 +14,7 @@ class EmployeeForm(forms.ModelForm):
 
     class Meta:
         model = Employee
-        fields = ['employee_id', 'name', 'department', 'job_title', 'rank']
+        fields = ['employee_id', 'name', 'department', 'job_title', 'rank', 'money_per_hour']
         widgets = {
             'employee_id': forms.NumberInput(
                 attrs={
@@ -27,6 +27,12 @@ class EmployeeForm(forms.ModelForm):
             'department': forms.Select(attrs={'class': 'form-control'}),
             'job_title': forms.Select(attrs={'class': 'form-control'}),
             'rank': forms.Select(attrs={'class': 'form-control'}),
+            'money_per_hour': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'type': 'number',
+                'min': '0',
+                'step': '0.01'
+            }),
         }
 
 
@@ -40,9 +46,15 @@ class UpdateEmployeeForm(forms.ModelForm):
         
     class Meta:
         model = Employee
-        fields = ['name', 'job_title', 'rank']
+        fields = ['name', 'job_title', 'rank', 'money_per_hour']
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'job_title': forms.Select(attrs={'class': 'form-control'}),
             'rank': forms.Select(attrs={'class': 'form-control'}),
+            'money_per_hour': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'type': 'number',
+                'min': '0',
+                'step': '0.01'
+            }),
         }
