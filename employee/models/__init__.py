d3c92bf18886a63c7873f6ceb74a86ad1b262529
onongol <rozserbatlin@gmail.com>
2025-10-23 11:34:02 +0800

    Import DailyWork model in the employee models package

--- employee/models/__init__.py
+++ employee/models/__init__.py
@@ -1,6 +1,7 @@
 from .employee_models import Employee
 from .work_models import Work
 from .daily_salary_models import DailySalary
+from .daily_work_models import DailyWork
 from .piecework_models import Piecework
 from .master_models import Master
 from .payroll_models import Payroll
