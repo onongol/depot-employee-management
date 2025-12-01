16ea69330be9a92841c9467770b276d1d92f160b
onongol <rozserbatlin@gmail.com>
2025-12-01 15:14:00 +0800

    Refactor WorkForm to inherit from WorkFormMixin for improved code reuse and maintainability

--- employee/forms/work_forms.py
+++ employee/forms/work_forms.py
@@ -5,8 +5,7 @@ from employee.models import Work
 from employee.constants.constants import get_job_title_choices, TYPE_WAGON_CHOICES, ALLOWED_WAGON_DEPARTMENTS, EMPTY_SELECT
 
 
-class WorkForm(forms.ModelForm):
-    """Form for creating or updating a Work record."""
+class WorkFormMixin:
     def __init__(self, *args, **kwargs):
         """Initialize form and dynamically set job title and type_wagon choices based on department."""
         # Get the department from the keyword arguments
@@ -49,6 +48,12 @@ class WorkForm(forms.ModelForm):
                     _('The Work Name must be unique within the department.')
                 )
         return work_name
+
+class WorkForm(WorkFormMixin, forms.ModelForm):
+    """Form for creating or updating a Work record."""
+    def __init__(self, *args, **kwargs):
+        """Initialize form and dynamically set job title and type_wagon choices based on department."""
+        super().__init__(*args, **kwargs)
 
     class Meta:
         model = Work
@@ -90,46 +95,7 @@ class UpdateWorkForm(forms.ModelForm):
     """Form for updating an existing Work record."""
     def __init__(self, *args, **kwargs):
         """Initialize form and dynamically set job title and type_wagon choices based on department."""
-        # Get the department from the keyword arguments
-        department = kwargs.pop('department', None) 
         super().__init__(*args, **kwargs)
-        dept = (
-            department
-            or (self.data.get('department') if self.is_bound else None)
-            or getattr(self.instance, 'department', None)
-        )
-        # Dynamically set job title choices based on department
-        self.fields['job_title'].choices = get_job_title_choices(dept) 
-
-        # Dynamically set type_wagon choices based on department
-        if dept in set(ALLOWED_WAGON_DEPARTMENTS):
-            self.fields['type_wagon'].choices = EMPTY_SELECT + list(TYPE_WAGON_CHOICES)
-            self.fields['type_wagon'].initial = None
-        else:
-            # Show empty select (not required) and ensure None
-            self.fields['type_wagon'].choices = EMPTY_SELECT
-            self.fields['type_wagon'].initial = None
-
-    def clean_work_name(self):
-        """Ensure work_name is unique within the department."""
-        cleaned_data = super().clean()
-        work_name = cleaned_data.get('work_name')
-        department = (
-            self.cleaned_data.get('department')
-            or self.initial.get('department')
-            or (self.data.get('department') if self.is_bound else None)
-            or getattr(self.instance, 'department', None)
-        )
-        if work_name and department:
-            qs = Work.objects.filter(work_name=work_name, department=department)
-            if self.instance.pk:
-                qs = qs.exclude(pk=self.instance.pk)
-            if qs.exists():
-                self.add_error(
-                    'work_name',
-                    _('The Work Name must be unique within the department.')
-                )
-        return work_name
     
     class Meta:
         model = Work
