9cf11eecce38d8ea9029c6eee0f5a187899f5708
onongol <rozserbatlin@gmail.com>
2025-12-03 16:23:03 +0800

    Add docstrings to permission mixins and functions for clarity

--- employee/utils/permissions.py
+++ employee/utils/permissions.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 
 from employee.constants.constants import GroupNames
 
+
 class OnlyGroupMixin(UserPassesTestMixin):
     """Mixin to restrict access to users in specific groups or superusers."""
     group_names = []
@@ -12,16 +13,20 @@ class OnlyGroupMixin(UserPassesTestMixin):
 
 
 def is_admin(user):
+    """Check if user is in Payrolls group or is a superuser."""
     return user.is_superuser or user.groups.filter(name__in=[GroupNames.PAYROLLS.value]).exists()
 
 
 class OnlyAdminMixin(OnlyGroupMixin):
+    """Mixin to restrict access to users in Payrolls group or superusers."""
     group_names = [GroupNames.PAYROLLS.value]
 
 
 def is_creater(user):
+    """Check if user is in Payrolls or Masters groups or is a superuser."""
     return user.is_superuser or user.groups.filter(name__in=[GroupNames.PAYROLLS.value, GroupNames.MASTERS.value]).exists()
 
 
 class OnlyCreaterMixin(OnlyGroupMixin):
+    """Mixin to restrict access to users in Payrolls or Masters groups or superusers."""
     group_names = [GroupNames.PAYROLLS.value, GroupNames.MASTERS.value]
