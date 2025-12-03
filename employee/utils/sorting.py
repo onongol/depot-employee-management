2726ae46761c0f5c9d8dc379e954d4ee1e570ec6
onongol <rozserbatlin@gmail.com>
2025-12-03 15:25:50 +0800

    Enhance apply_ordering docstring to clarify default parameter usage

--- employee/utils/sorting.py
+++ employee/utils/sorting.py
@@ -5,10 +5,15 @@ def apply_ordering(queryset, order_by, direction, allowed_fields, default='-reco
     :param order_by: field name to order by (str)
     :param direction: 'asc' or 'desc'
     :param allowed_fields: list of allowed fields for ordering
-    :param default: default ordering field
+    :param default: default ordering field or list/tuple of fields
     :return: ordered QuerySet
     """
     if order_by in allowed_fields:
         sort_field = f"-{order_by}" if direction == 'desc' else order_by
         return queryset.order_by(sort_field)
+    
+    # Support default as string or iterable of fields
+    if isinstance(default, (list, tuple)):
+        return queryset.order_by(*default)
+    
     return queryset.order_by(default)
