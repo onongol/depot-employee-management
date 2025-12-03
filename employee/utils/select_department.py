19b5d2fe4a43d38b8edbe06cb72b6753486092a7
onongol <rozserbatlin@gmail.com>
2025-12-03 16:28:28 +0800

    Remove unnecessary blank line in get_selected_department function

--- employee/utils/select_department.py
+++ employee/utils/select_department.py
@@ -1,4 +1,3 @@
-
 def get_selected_department(request):
     """Get the selected department from request or session."""
     return request.GET.get('department') or request.session.get('department')
