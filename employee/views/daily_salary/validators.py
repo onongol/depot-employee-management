087542989edba458d83619c4cdb73a2f88f6147f
onongol <rozserbatlin@gmail.com>
2025-12-04 11:21:45 +0800

    Add validation function for required fields in daily salary creation

---
+++ employee/views/daily_salary/validators.py
@@ -0,0 +1,16 @@
+from django.utils.translation import gettext_lazy as _
+
+
+def validate_required_daily_salary(selected_ids, salary_date, hours_per_day):
+    """Validate required fields for daily salary creation."""
+    errors = []
+
+    # Check if any employees are selected
+    if not selected_ids:
+        errors.append(_("Please select at least one employee."))
+
+    # Validate required fields
+    if not salary_date or not hours_per_day:
+        errors.append(_("Please select date and hours!"))
+
+    return errors
