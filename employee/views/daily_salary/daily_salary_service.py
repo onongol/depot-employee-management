9afcafe7eba7814af728fcefad49ebab6f180b99
onongol <rozserbatlin@gmail.com>
2025-12-05 14:32:56 +0800

    Add department field to DailySalary records creation

--- employee/views/daily_salary/daily_salary_service.py
+++ employee/views/daily_salary/daily_salary_service.py
@@ -43,6 +43,7 @@ def create_daily_salary_records(selected_ids, salary_date, hours_per_day):
                 DailySalary(
                     employee_id=emp_id,
                     employee_name=emp.name,
+                    department=emp.department,
                     salary_date=salary_date,
                     hours_per_day=hours_per_day,
                     salary_day=salary_day
