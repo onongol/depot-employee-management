19ce01a0ec037ba9322773f8d083ce4cf4c492a4
onongol <rozserbatlin@gmail.com>
2025-12-10 16:36:31 +0800

    Add build_headers function for DailyWork export header generation

---
+++ employee/views/daily_work/daily_work_export/build_headers.py
@@ -0,0 +1,28 @@
+from django.utils.translation import gettext_lazy as _
+
+from employee.constants.constants import ALLOWED_WAGON_DEPARTMENTS
+
+
+def build_headers(department):
+    """Build headers for DailyWork export based on department."""
+    headers = [
+        _("#"),
+        _("Work"),
+        _("Position"),
+        _("Type"),
+    ]
+    if department in ALLOWED_WAGON_DEPARTMENTS:
+        headers += [
+            _("Wagon"), 
+            _("Type Wagon")
+        ]
+    headers += [
+        _("Amount"), 
+        _("Time"), 
+        _("Price"), 
+        _("Date")
+    ]
+
+    headers = [str(h) for h in headers]
+
+    return headers
