86ccc0a2cd2bd62d597e388f6e76f7ca5ded5453
onongol <rozserbatlin@gmail.com>
2025-12-11 10:27:20 +0800

    Add FormDepartmentMixin to inject selected department into form kwargs

---
+++ employee/mixins/department_form_mixins.py
@@ -0,0 +1,9 @@
+from employee.utils.select_department import get_selected_department
+
+
+class FormDepartmentMixin:
+    """Inject selected department into form kwargs."""
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['department'] = get_selected_department(self.request)
+        return kwargs
