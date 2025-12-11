43d2f22bac2167f5b417701b8e500dddeb54c96b
onongol <rozserbatlin@gmail.com>
2025-12-11 11:49:37 +0800

    Add build_headers function for Employee Salary export headers

---
+++ employee/views/employee_salary/employee_salary_export/build_headers.py
@@ -0,0 +1,23 @@
+from django.utils.translation import gettext_lazy as _
+
+
+def build_headers():
+    """Build headers for Employee Salary export based on department."""
+    headers = [
+        _("#"),
+        _("ID"),
+        _("Name"),
+        _("Department"),
+        _("Position"),
+        _("Rank"),
+        #_("Month Salary"),
+        _("Time"),
+        _("Salary"),
+        #_("Total Salary"),
+        _("Month"),
+        _("Year"),
+    ]
+
+    headers = [str(h) for h in headers]
+
+    return headers
