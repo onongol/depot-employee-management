2f1be6755b4a53690a91a49edd82c73567f99a06
onongol <rozserbatlin@gmail.com>
2025-12-11 09:21:53 +0800

    Fix import path for GenericContextMixin in context_mixins.py

--- employee/mixins/context_mixins.py
+++ employee/mixins/context_mixins.py
@@ -1,7 +1,7 @@
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
-from .mixins import GenericContextMixin
+from .generic_mixins import GenericContextMixin
 from employee.models import Employee, Work, DailySalary, DailyWork, Piecework
 
 
