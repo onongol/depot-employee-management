01d0b34a77eeb90d2c5afa454de71ad92f333fe5
onongol <rozserbatlin@gmail.com>
2025-12-11 13:50:22 +0800

    Add build_totals_row function to generate totals for DailyWork export

---
+++ employee/views/wagon/wagon_export/build_totals_row.py
@@ -0,0 +1,18 @@
+from django.utils.translation import gettext_lazy as _
+
+from ..wagon_totals import get_totals
+
+
+def build_totals_row(qs):
+    """Build totals row for DailyWork export based on department."""
+    # Calculate totals  
+    totals = get_totals(qs)
+
+    total_str = str(_("Total"))
+    empty = ""
+
+    empty_cols = 4
+
+    totals_row = [total_str] + [empty] * empty_cols + [totals['total_amount'], totals['total_time'], totals['total_price']] + [empty]
+    
+    return totals_row
