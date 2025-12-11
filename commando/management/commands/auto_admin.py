278284f6e7a948feddaaaa8c043d59842e3b1cd4
onongol <rozserbatlin@gmail.com>
2025-12-11 16:40:16 +0800

    Fix import order in auto_admin.py for consistency

--- commando/management/commands/auto_admin.py
+++ commando/management/commands/auto_admin.py
@@ -1,9 +1,8 @@
 from typing import Any
 
+from decouple import config
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
-
-from decouple import config
 
 
 class Command(BaseCommand):
