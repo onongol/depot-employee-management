bfcafb547f63127102efcdb4077d7f3214eacbf4
onongol <rozserbatlin@gmail.com>
2025-12-11 13:50:12 +0800

    Add build_headers function for Employee Salary export headers

---
+++ employee/views/wagon/wagon_export/build_headers.py
@@ -0,0 +1,20 @@
+from django.utils.translation import gettext_lazy as _
+
+
+def build_headers():
+    """Build headers for Employee Salary export based on department."""
+    headers = [
+        _("#"),
+        _("Wagon Number"),
+        _("Type Wagon"),
+        _("Work Name"),
+        _("Type Work"),
+        _("Amount"),
+        _("Total Time"),
+        _("Total Price"),
+        _("Date"),
+    ]
+
+    headers = [str(h) for h in headers]
+
+    return headers
