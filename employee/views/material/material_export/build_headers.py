99a09ac6816bb3d6f6113d75e06b7283552729e1
onongol <rozserbatlin@gmail.com>
2025-12-11 15:26:06 +0800

    Add build_headers function to generate localized headers for material export

---
+++ employee/views/material/material_export/build_headers.py
@@ -0,0 +1,15 @@
+from django.utils.translation import gettext_lazy as _
+
+
+def build_headers():
+    headers = [
+        _("#"),
+        _("Type Material"),
+        _("Work Name"), 
+        _("Amount Material"),
+        _("Date"),
+    ]
+
+    headers = [str(h) for h in headers]
+
+    return headers
