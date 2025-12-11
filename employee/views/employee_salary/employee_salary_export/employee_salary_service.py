b8ea2c31cb3270fe9c4d53775cd404026f3b3a6b
onongol <rozserbatlin@gmail.com>
2025-12-11 13:48:59 +0800

    Add get_employee_salaries function to retrieve and filter employee salary data

--- employee/views/employee_salary/employee_salary_export/employee_salary_service.py
+++ employee/views/employee_salary/employee_salary_export/employee_salary_service.py
@@ -1,9 +1,9 @@
 from employee.utils.filters import filter_employees
-from .employee_salary_prepare import employee_salaries_prepare
-from .employee_salary_calculate import employee_salary_calculate
+from ..employee_salary_prepare import employee_salaries_prepare
+from ..employee_salary_calculate import employee_salary_calculate
 
 
-def get_filtered_employee_salaries(request):
+def get_employee_salaries(request):
     """Return a list of filtered employee salaries based on request parameters."""
     # Prepare the base queryset and filter parameters
     employees, employee_id, employee_name, department, job_title, month, year, month_period = employee_salaries_prepare(request)
