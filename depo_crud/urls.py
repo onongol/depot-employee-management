e1df36d2deaea02a620ed05b1594b4a0d89a3067
onongol <rozserbatlin@gmail.com>
2025-12-11 16:40:28 +0800

    Fix import order in urls.py for consistency

--- depo_crud/urls.py
+++ depo_crud/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
+from django.contrib import admin
+from django.urls import include, path
 from django.views.i18n import JavaScriptCatalog
- 
+
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
