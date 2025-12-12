70b1ebfe991eecacc1a5ae0be680d80c91f7d283
onongol <rozserbatlin@gmail.com>
2025-12-12 10:12:08 +0800

    Add send_delete_warning function to notify users of deleted objects and related records

---
+++ employee/messages/delete_warning.py
@@ -0,0 +1,12 @@
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _ 
+
+
+def send_delete_warning(request, object_name):
+    """Send a warning message after deleting an object."""
+    messages.warning(
+        request,
+        _('You have deleted %(object_name)s. Also, all related records have been deleted.') % {
+            'object_name': object_name
+        }
+    )
