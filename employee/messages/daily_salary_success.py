f4fdb8bf18c85b06f2e5ca72761ed56d864bd83b
onongol <rozserbatlin@gmail.com>
2025-12-12 10:11:45 +0800

    Add function to send success messages for DailySalary record creation

---
+++ employee/messages/daily_salary_success.py
@@ -0,0 +1,23 @@
+from django.contrib import messages
+from django.utils.translation import gettext as _
+
+
+def send_daily_salary_creation_message(request, *, employees_dict, selected_ids, salary_date, template: str | None = None):
+    """
+    Send a success message for created DailySalary records.
+    - employees_dict: mapping employee_id -> Employee instance
+    - selected_ids: list of employee_ids attempted to create
+    - salary_date: date string
+    """
+    if template is None:
+        template = _("Daily salary records %(employees)s on %(date)s created successfully.")
+
+    employee_list = [
+        f"{emp.employee_id}/{emp.name}"
+        for emp_id, emp in employees_dict.items()
+        if emp_id in selected_ids
+    ]
+
+    employees_str = ", ".join(sorted(employee_list)) if employee_list else ""
+    
+    messages.success(request, template % {'employees': employees_str, 'date': salary_date})
