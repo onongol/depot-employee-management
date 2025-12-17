061de9957c173a6acafb859ce1aeae0b6665b161
onongol <rozserbatlin@gmail.com>
2025-12-17 10:29:22 +0800

    Fix import path for export_to_excel utility function

--- employee/views/employee_salary/employee_salary_export/employee_salary_excel.py
+++ employee/views/employee_salary/employee_salary_export/employee_salary_excel.py
@@ -1,6 +1,6 @@
 from django.utils.translation import gettext_lazy as _
 
-from employee.utils.export_excel import export_to_excel
+from employee.utils.exports.export_excel import export_to_excel
 from employee.views.employee_salary.employee_salary_export.build_headers import build_headers
 from employee.views.employee_salary.employee_salary_export.employee_salary_service import get_employee_salaries
 from employee.views.employee_salary.employee_salary_export.format_data import iter_rows
