1bd92bd856fd055c45eb2a8e03c36307951134c3
onongol <rozserbatlin@gmail.com>
2025-12-17 10:35:45 +0800

    Simplify __all__ declaration in employee models by importing all classes directly

--- employee/models/__init__.py
+++ employee/models/__init__.py
@@ -1,17 +1,7 @@
-from .employee_models import Employee
-from .work_models import Work
-from .daily_salary_models import DailySalary
-from .daily_work_models import DailyWork
-from .piecework_models import Piecework
-from .master_models import Master
-from .payroll_models import Payroll
-
-__all__ = [
-    "Employee",
-    "Work",
-    "DailySalary",
-    "DailyWork",
-    "Piecework",
-    "Master",
-    "Payroll",
-]
+from .employee_models import *
+from .work_models import *
+from .daily_salary_models import *
+from .daily_work_models import *
+from .piecework_models import *
+from .master_models import *
+from .payroll_models import *
