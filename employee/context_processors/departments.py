6a390d63e0b4636387815d5b08e80b16da265566
onongol <rozserbatlin@gmail.com>
2025-12-17 09:18:19 +0800

    Add global_departments context processor to provide distinct department list

---
+++ employee/context_processors/departments.py
@@ -0,0 +1,13 @@
+from employee.constants.constants import ALLOWED_WAGON_DEPARTMENTS, DEPARTMENTS
+
+
+def global_departments(request):
+    """Context processor to provide a list of distinct departments."""
+    departments_list = DEPARTMENTS
+    if not departments_list:
+        departments_list = ['No departments available']
+    return {
+        'departments': departments_list,
+        'ALLOWED_WAGON_DEPARTMENTS': ALLOWED_WAGON_DEPARTMENTS,
+        'request': request,
+    }
