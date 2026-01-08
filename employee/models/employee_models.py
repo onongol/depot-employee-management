494a2055d794d77d285d7c7164d60c8284513715
onongol <rozserbatlin@gmail.com>
2026-01-08 13:40:55 +0800

    Update money_per_hour field validator to require a minimum value of 0.01

--- employee/models/employee_models.py
+++ employee/models/employee_models.py
@@ -40,7 +40,7 @@ class Employee(models.Model):
     money_per_hour = models.DecimalField(
         max_digits=20, 
         decimal_places=2,
-        validators=[MinValueValidator(0)], 
+        validators=[MinValueValidator(0.01)], 
         null=False, 
         editable=True
     )
