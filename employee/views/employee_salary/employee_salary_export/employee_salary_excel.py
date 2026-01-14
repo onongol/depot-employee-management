234036084a511dd50905ebbde7e846b10bdf209c
onongol <rozserbatlin@gmail.com>
2026-01-14 11:38:33 +0800

    Refactor employee_salary_export_excel function to include wagon_mode for conditional data export

--- employee/views/employee_salary/employee_salary_export/employee_salary_excel.py
+++ employee/views/employee_salary/employee_salary_export/employee_salary_excel.py
@@ -1,20 +1,24 @@
 from django.utils.translation import gettext_lazy as _
 
+from employee.constants.constants import GROUP_WAGON
 from employee.utils.exports.export_excel import export_to_excel
 from employee.views.employee_salary.employee_salary_export.build_headers import build_headers
-from employee.views.employee_salary.employee_salary_export.employee_salary_service import get_employee_salaries
 from employee.views.employee_salary.employee_salary_export.format_data import iter_rows
+from employee.views.employee_salary.employee_salary_export.employee_salary_service import get_employee_salaries
 
 
 def employee_salary_export_excel(request):
     """Export employee salaries data to Excel."""
-    employee_salaries = get_employee_salaries(request)
+    employee_salaries, group, wagon_mode = get_employee_salaries(request)
 
-    headers = build_headers()
-    
-    data = list(iter_rows(employee_salaries))
+    headers = build_headers(show_wagon=wagon_mode)
+    data = list(iter_rows(employee_salaries, show_wagon=wagon_mode))
 
-    file_name = "employee_salaries.xlsx"
-    sheet_title = str(_("Employee Salaries"))
+    meta = {
+        GROUP_WAGON: ("employee_salaries_wagon.xlsx", _("Employee Salaries by Wagon")),
+    }
+
+    file_name, title = meta.get(group, ("employee_salaries.xlsx", _("Employee Salaries")))
+    sheet_title = str(title)
 
     return export_to_excel(data, headers, file_name, sheet_title)
