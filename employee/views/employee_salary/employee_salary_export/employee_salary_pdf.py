b9dbb8c8e48b6ca4f04eca8e3f3b27d2d736dcd6
onongol <rozserbatlin@gmail.com>
2026-01-14 11:38:43 +0800

    Refactor employee_salary_export_pdf function to include wagon_mode for conditional data handling and improve column definitions

--- employee/views/employee_salary/employee_salary_export/employee_salary_pdf.py
+++ employee/views/employee_salary/employee_salary_export/employee_salary_pdf.py
@@ -1,28 +1,53 @@
 from django.utils.translation import gettext_lazy as _
 
+from employee.constants.constants import GROUP_WAGON
 from employee.utils.exports.export_pdf import export_to_pdf
 from employee.views.employee_salary.employee_salary_export.build_headers import build_headers
-from employee.views.employee_salary.employee_salary_export.employee_salary_service import get_employee_salaries
 from employee.views.employee_salary.employee_salary_export.format_data import iter_rows
+from employee.views.employee_salary.employee_salary_export.employee_salary_service import get_employee_salaries
 
 
 def employee_salary_export_pdf(request):
     """Export employee salaries data to PDF."""
-    employee_salaries = get_employee_salaries(request)
+    employee_salaries, group, wagon_mode = get_employee_salaries(request)
 
-    headers = build_headers()
+    headers = build_headers(show_wagon=wagon_mode)
 
-    # A4 Landscape ~842pt; usable width ~810pt
-    col_widths = [30, 30, 150, 150, 150, 50, 50, 100, 50, 50]
+    # A4 Landscape ~842pt;
+    columns = ["#", "id", "name", "department", "position", "rank"]
+    if wagon_mode:
+        columns.append("wagon")
+    columns += ["time", "salary", "month", "year"]
+
+    widths_by_key = {
+        "#": 30,
+        "id": 30,
+        "name": 100,
+        "department": 150,
+        "position": 100,
+        "rank": 50,
+        "wagon": 80,
+        "time": 50,
+        "salary": 100,
+        "month": 50,
+        "year": 50,
+    }
+
+    col_widths = [widths_by_key[col] for col in columns]
     
     # Define column alignments
+    last_col = len(col_widths) - 1
     col_alignments = [
-        ('ALIGN', (0, 1), (9, -1), 'LEFT'), # Align all columns to LEFT
+        ('ALIGN', (0, 1), (last_col, -1), 'LEFT'),
     ]
 
-    data = list(iter_rows(employee_salaries))
+    data = list(iter_rows(employee_salaries, show_wagon=wagon_mode))
 
-    file_name = "employee_salaries.pdf"
-    sheet_title = str(_("Employee Salaries"))
+    meta = {
+        GROUP_WAGON: ("employee_salaries_wagon.pdf", _("Employee Salaries by Wagon")),
+    }
+
+    file_name, title = meta.get(group, ("employee_salaries.pdf", _("Employee Salaries")))
+    sheet_title = str(title)
 
     return export_to_pdf(data, headers, col_widths, col_alignments, sheet_title, file_name)
