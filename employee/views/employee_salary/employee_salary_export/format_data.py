e9b5443d6ccac02d02bcbb6ad56290cf685963a4
onongol <rozserbatlin@gmail.com>
2026-01-14 11:38:28 +0800

    Refactor iter_rows function to include show_wagon parameter for conditional wagon number inclusion

--- employee/views/employee_salary/employee_salary_export/format_data.py
+++ employee/views/employee_salary/employee_salary_export/format_data.py
@@ -1,17 +1,26 @@
-def iter_rows(qs):
+from employee.constants.constants import DEFAULT_WAGON_NUMBER
+
+
+def iter_rows(qs, *, show_wagon: bool = False):
     for i, item in enumerate(qs, start=1):
         row = [
             i,
-            item['employee'].employee_id or "",
-            item['employee'].name or "",
-            item['employee'].department or "",
-            item['employee'].job_title or "",
-            item['employee'].rank or "",
-            item['total_piecework_time'] or 0,
-            #item['total_salary_day'] or 0,
-            item['total_piecework_amount'] or 0,
-            #item['total_salary'] or 0,
-            item['month'] or "",
-            item['year'] or "",
+            item["employee"].employee_id or "",
+            item["employee"].name or "",
+            item["employee"].department or "",
+            item["employee"].job_title or "",
+            item["employee"].rank or "",
         ]
+
+        if show_wagon:
+            wagon = item.get("wagon_number")
+            row.append(wagon if wagon else DEFAULT_WAGON_NUMBER)
+
+        row += [
+            item.get("total_piecework_time") or 0,
+            item.get("total_piecework_amount") or 0,
+            item.get("month") or "",
+            item.get("year") or "",
+        ]
+
         yield row
