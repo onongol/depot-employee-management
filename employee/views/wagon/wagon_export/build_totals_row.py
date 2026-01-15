3d4bdd3e94f6f77633db744670c99183b63ecc74
onongol <rozserbatlin@gmail.com>
2026-01-15 10:36:15 +0800

    Refactor build_totals_row function to accept totals as a parameter and group for dynamic row generation based on grouping criteria.

--- employee/views/wagon/wagon_export/build_totals_row.py
+++ employee/views/wagon/wagon_export/build_totals_row.py
@@ -1,17 +1,30 @@
 from django.utils.translation import gettext_lazy as _
 
-from employee.views.wagon.wagon_totals import get_totals
+from employee.constants.constants import GROUP_MONTH
 
 
-def build_totals_row(qs):
-    # Calculate totals  
-    totals = get_totals(qs)
-
+def build_totals_row(totals: dict, group=None):
     total_str = str(_("Total"))
     empty = ""
 
-    empty_cols = 4
+    total_amount = totals.get("total_amount") or 0
+    total_time = totals.get("total_time") or 0
+    total_price = totals.get("total_price") or 0
 
-    totals_row = [total_str] + [empty] * empty_cols + [totals['total_amount'], totals['total_time'], totals['total_price']] + [empty]
+    if group == GROUP_MONTH:
+        empty_cols = 2
+        tail_empties = 2
+        totals_cells = [total_time, total_price]
+    else:
+        empty_cols = 4
+        tail_empties = 1
+        totals_cells = [total_amount, total_time, total_price]
+
+    totals_row = (
+        [total_str] 
+        + [empty] * empty_cols 
+        + totals_cells 
+        + [empty] * tail_empties
+    )
     
     return totals_row
