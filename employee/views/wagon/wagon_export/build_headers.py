0d22c1ef64b34794b28f13924fde0b2036663e8f
onongol <rozserbatlin@gmail.com>
2026-01-15 10:36:10 +0800

    Refactor build_headers function to accept a group parameter for dynamic header generation based on grouping criteria.

--- employee/views/wagon/wagon_export/build_headers.py
+++ employee/views/wagon/wagon_export/build_headers.py
@@ -1,18 +1,31 @@
 from django.utils.translation import gettext_lazy as _
 
+from employee.constants.constants import GROUP_MONTH
 
-def build_headers():
-    headers = [
-        ("#"),
-        _("Wagon Number"),
-        _("Type Wagon"),
-        _("Work Name"),
-        _("Type Work"),
-        _("Amount"),
-        _("Total Time"),
-        _("Total Price"),
-        _("Date"),
-    ]
+
+def build_headers(group=None):
+    if group == GROUP_MONTH:
+        headers = [
+            ("#"),
+            _("Wagon Number"),
+            _("Type Wagon"),
+            _("Total Time"),
+            _("Total Price"),
+            _("Month"),
+            _("Year"),
+        ]
+    else:
+        headers = [
+            ("#"),
+            _("Wagon Number"),
+            _("Type Wagon"),
+            _("Work Name"),
+            _("Type Work"),
+            _("Amount"),
+            _("Total Time"),
+            _("Total Price"),
+            _("Date"),
+        ]
 
     headers = [str(h) for h in headers]
 
