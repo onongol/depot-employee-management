351b5a6c67b695be9090af7c5ec057b55aa8d95e
onongol <rozserbatlin@gmail.com>
2026-01-19 13:33:54 +0800

    Add block_bulk_messages function to display user feedback for blocked bulk deletions

---
+++ employee/views/daily_salary/daily_salary_delete_bulk/messages/block_bulk_messages.py
@@ -0,0 +1,19 @@
+from django.contrib import messages
+from django.utils.translation import gettext_lazy as _
+
+
+def block_bulk_messages(
+    request, 
+    blocked_count, 
+    blocked_items, 
+    blocked_tail
+):  
+    '''Shows a success message after bulk deletion, including a preview of deleted items for clear user feedback.'''
+    if blocked_count:
+        messages.info(
+            request,
+            _(
+                "Cannot delete %(count)s record(s) because they are associated with Daily Work / Piecework: %(items)s%(tail)s"
+            )
+            % {"count": blocked_count, "items": ", ".join(blocked_items), "tail": blocked_tail},
+        )
