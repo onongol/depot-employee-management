bfdbe3d1877096bf1889283a7650a4916ce89900
onongol <rozserbatlin@gmail.com>
2026-01-20 15:04:40 +0800

    Import daily_work_delete_bulk module in daily_work views

--- employee/views/daily_work/__init__.py
+++ employee/views/daily_work/__init__.py
@@ -1,3 +1,4 @@
 from .daily_work_views import *
 from .daily_work_list import *
 from .daily_work_export.daily_work_export_excel import *
+from .daily_work_delete_bulk.daily_work_delete_bulk import *
