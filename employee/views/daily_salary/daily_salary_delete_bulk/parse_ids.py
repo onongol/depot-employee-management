be7cb8d3be289c4ec09a5c9ebcfeb9cb5d81a34a
onongol <rozserbatlin@gmail.com>
2026-01-20 15:05:38 +0800

    Remove parse_ids function for bulk deletion of DailySalary records

--- employee/views/daily_salary/daily_salary_delete_bulk/parse_ids.py
+++
@@ -1,9 +0,0 @@
-def parse_ids(raw_ids):
-    """Handles bulk deletion of DailySalary via checkboxes, blocking deletions linked to Piecework, and displays user feedback messages."""
-    ids = []
-    for x in raw_ids:
-        try:
-            ids.append(int(x))
-        except (TypeError, ValueError):
-            continue
-    return ids
