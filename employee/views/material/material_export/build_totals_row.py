74da3310fd1b850eeaaf7321d15ca4d51f1e22d1
onongol <rozserbatlin@gmail.com>
2026-01-22 09:34:34 +0800

    Refactor build_totals_row.py for consistent string quoting

--- employee/views/material/material_export/build_totals_row.py
+++ employee/views/material/material_export/build_totals_row.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 def build_totals_row(qs):
-    total_amount = sum(item['amount_material'] for item in qs) if qs else 0
+    total_amount = sum(item["amount_material"] for item in qs) if qs else 0
 
     total_str = str(_("Total"))
     empty = ""
@@ -10,5 +10,5 @@ def build_totals_row(qs):
     empty_cols = 2
 
     totals_row = [total_str] + [empty] * empty_cols + [total_amount] + [empty]
-    
+
     return totals_row
