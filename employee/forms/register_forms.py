796ef1c51401de43e9b375468556f367c371dae3
onongol <rozserbatlin@gmail.com>
2026-01-22 10:22:58 +0800

    Normalize string quoting and improve formatting in register_forms.py

--- employee/forms/register_forms.py
+++ employee/forms/register_forms.py
@@ -6,18 +6,21 @@ from django.utils.translation import gettext_lazy as _
 
 class CustomUserCreationForm(UserCreationForm):
     """User registration form with additional employee_id field."""
+
     employee_id = forms.IntegerField(
         label=_("Employee ID"),
         required=True,
         min_value=1,
-        widget=forms.NumberInput(attrs={
-            'class': 'form-control',
-            'type': 'number',
-            'min': '1',
-        }),
-        help_text=_("Enter your Employee ID to link your account.")
+        widget=forms.NumberInput(
+            attrs={
+                "class": "form-control",
+                "type": "number",
+                "min": "1",
+            }
+        ),
+        help_text=_("Enter your Employee ID to link your account."),
     )
-    
+
     class Meta:
         model = User
         fields = ("employee_id", "username", "password1", "password2")
