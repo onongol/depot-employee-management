1082d88bb63b16a0bcf42b016b239ac7fcde4121
onongol <rozserbatlin@gmail.com>
2026-01-22 09:47:16 +0800

    Swap import order between piecework_views and piecework_excel for better organization

--- employee/views/piecework/__init__.py
+++ employee/views/piecework/__init__.py
@@ -1,4 +1,4 @@
-from .piecework_views import *
+from .piecework_export.piecework_excel import *
 from .piecework_list import *
 from .piecework_update import *
-from .piecework_export.piecework_excel import *
+from .piecework_views import *
