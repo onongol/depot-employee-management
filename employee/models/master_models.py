22bd73faec196e67674604ef698d753e1008a227
onongol <rozserbatlin@gmail.com>
2026-01-22 10:34:20 +0800

    Refactor Master model for improved readability and consistency

--- employee/models/master_models.py
+++ employee/models/master_models.py
@@ -7,32 +7,23 @@ from employee.constants.constants import DEPARTMENT_CHOICES
 
 class Master(models.Model):
     """This model represents a master in the system."""
+
     master_id = models.IntegerField(
-        primary_key=True,
-        null=False, 
-        validators=[MinValueValidator(1)], 
-        unique=True
+        primary_key=True, null=False, validators=[MinValueValidator(1)], unique=True
     )
-    name = models.CharField(
-        max_length=255
-    )
-    department = models.CharField(
-        max_length=255, 
-        choices=DEPARTMENT_CHOICES
-    )
+    name = models.CharField(max_length=255)
+    department = models.CharField(max_length=255, choices=DEPARTMENT_CHOICES)
 
     # Active status of the employee
-    is_active = models.BooleanField(
-        default=True
-    )
+    is_active = models.BooleanField(default=True)
 
     # Connection to the User model
     user = models.OneToOneField(
-        User, 
-        on_delete=models.SET_NULL, 
-        null=True, 
-        blank=True, 
-        related_name='master_profile'
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="master_profile",
     )
 
     def __str__(self):
