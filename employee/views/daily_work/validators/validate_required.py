04b0f885ae7b169c6d779e56aecb84e4cbe4bdcf
onongol <rozserbatlin@gmail.com>
2026-01-22 09:09:42 +0800

    Refactor validate_required.py for improved code readability and consistency

--- employee/views/daily_work/validators/validate_required.py
+++ employee/views/daily_work/validators/validate_required.py
@@ -1,7 +1,9 @@
 from django.utils.translation import gettext_lazy as _
 
 
-def validate_required(selected_employee_ids, selected_work_ids, work_date, type_work, amounts):
+def validate_required(
+    selected_employee_ids, selected_work_ids, work_date, type_work, amounts
+):
     """Validate required fields and amounts for daily work entry."""
     errors = []
 
@@ -13,13 +15,11 @@ def validate_required(selected_employee_ids, selected_work_ids, work_date, type_
     if not work_date or not type_work:
         errors.append(_("Please select work date, type work."))
 
-    # Check for missing amounts for any selected work 
+    # Check for missing amounts for any selected work
     missing_amounts = [wid for wid in selected_work_ids if not amounts.get(wid)]
 
     # If there are missing amounts, add an error message
     if missing_amounts:
-        errors.append(
-            _("Please fill in the amount for selected work(s).")
-        )
+        errors.append(_("Please fill in the amount for selected work(s)."))
 
     return errors
