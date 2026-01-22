a4c6cdc2b8c4cf9002c1c8459222b7afac85f32b
onongol <rozserbatlin@gmail.com>
2026-01-22 09:48:29 +0800

    Refactor piecework_list view for improved readability and consistency in formatting

--- employee/views/piecework/piecework_list.py
+++ employee/views/piecework/piecework_list.py
@@ -1,19 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.utils.translation import gettext_lazy as _
 
-from employee.constants.constants import ALLOWED_WAGON_DEPARTMENTS, GROUP_MONTH, GROUP_YEAR
+from employee.constants.constants import (
+    ALLOWED_WAGON_DEPARTMENTS,
+    GROUP_MONTH,
+    GROUP_YEAR,
+)
 from employee.models import Piecework
 from employee.utils.filters import filter_pieceworks
 from employee.utils.pagination import paginate_queryset
 from employee.utils.select_type_wagon import get_type_wagon_filter_values
 from employee.utils.selects import get_distinct_values
 from employee.utils.totals import calc_totals_for_group
+from employee.views.piecework.group.group_and_sort import group_and_sort_pieceworks
 from employee.views.piecework.piecework_prepare import piecework_prepare
-from employee.views.piecework.group_and_sort import group_and_sort_pieceworks
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def piecework_list(request):
     """View to list all piecework records with filtering and pagination."""
     # Only show pieceworks for employees in the selected department
@@ -41,12 +44,21 @@ def piecework_list(request):
     ) = piecework_prepare(request)
 
     # Get distinct values for filtering dropdown
-    job_titles = get_distinct_values(Piecework, 'job_title', department, department_field='employee__department')
-    type_works = get_distinct_values(Piecework, 'type_work', department, department_field='work__department')
-    type_materials = get_distinct_values(Piecework, 'work__type_material', department, department_field='work__department')
+    job_titles = get_distinct_values(
+        Piecework, "job_title", department, department_field="employee__department"
+    )
+    type_works = get_distinct_values(
+        Piecework, "type_work", department, department_field="work__department"
+    )
+    type_materials = get_distinct_values(
+        Piecework,
+        "work__type_material",
+        department,
+        department_field="work__department",
+    )
 
     # Get snapshot values of type_wagon from Piecework
-    type_wagons = get_type_wagon_filter_values(department, source_model='piecework')
+    type_wagons = get_type_wagon_filter_values(department, source_model="piecework")
 
     # Years for Yearly filter dropdown
     years = [str(d.year) for d in pieceworks.dates("work_date", "year", order="DESC")]
@@ -63,7 +75,7 @@ def piecework_list(request):
         type_wagon=type_wagon,
         type_material=type_material,
         range_date=range_date,
-        record_date=record_date
+        record_date=record_date,
     )
 
     # Aggregation for totals
@@ -92,50 +104,50 @@ def piecework_list(request):
 
     # Prepare current filter values for template context
     filters = {
-        'employee_id': employee_id or '',
-        'employee_name': employee_name or '',
-        'job_title': job_title or '',
-        'work_name': work_name or '',
-        'type_work': type_work or '',
-        'wagon_number': wagon_number or '',
-        'type_wagon': type_wagon or '',
-        'type_material': type_material or '',
-        'range_date': range_date or '',
-        'record_date': record_date or '',
-        'group': group or '',
-        'month_period': month_period or '',
-        'year': selected_year or '',
+        "employee_id": employee_id or "",
+        "employee_name": employee_name or "",
+        "job_title": job_title or "",
+        "work_name": work_name or "",
+        "type_work": type_work or "",
+        "wagon_number": wagon_number or "",
+        "type_wagon": type_wagon or "",
+        "type_material": type_material or "",
+        "range_date": range_date or "",
+        "record_date": record_date or "",
+        "group": group or "",
+        "month_period": month_period or "",
+        "year": selected_year or "",
     }
 
     # Render the template with all context data
     return render(
         request,
-        'piecework/piecework_list.html',
-        {   
-            'ALLOWED_WAGON_DEPARTMENTS': ALLOWED_WAGON_DEPARTMENTS,
-            'GROUP_MONTH': GROUP_MONTH,
-            'GROUP_YEAR': GROUP_YEAR,
-            'employee_id': employee_id,
-            'employee_name': employee_name,
-            'job_title': job_title,
-            'work_name': work_name,
-            'type_work': type_work,
-            'wagon_number': wagon_number,
-            'type_wagon': type_wagon,
-            'range_date': range_date,
-            'record_date': record_date,
-            'pieceworks': page_obj,
-            'page_obj': page_obj,
-            'selected_department': department,
-            'type_works': type_works,
-            'type_materials': type_materials,
-            'job_titles': job_titles,
-            'type_wagons': type_wagons,
-            'totals': totals,
-            'filters': filters,
-            'group': group,
-            'month_period': month_period,
-            'years': years,
-            'selected_year': selected_year,
-        }
+        "piecework/piecework_list.html",
+        {
+            "ALLOWED_WAGON_DEPARTMENTS": ALLOWED_WAGON_DEPARTMENTS,
+            "GROUP_MONTH": GROUP_MONTH,
+            "GROUP_YEAR": GROUP_YEAR,
+            "employee_id": employee_id,
+            "employee_name": employee_name,
+            "job_title": job_title,
+            "work_name": work_name,
+            "type_work": type_work,
+            "wagon_number": wagon_number,
+            "type_wagon": type_wagon,
+            "range_date": range_date,
+            "record_date": record_date,
+            "pieceworks": page_obj,
+            "page_obj": page_obj,
+            "selected_department": department,
+            "type_works": type_works,
+            "type_materials": type_materials,
+            "job_titles": job_titles,
+            "type_wagons": type_wagons,
+            "totals": totals,
+            "filters": filters,
+            "group": group,
+            "month_period": month_period,
+            "years": years,
+            "selected_year": selected_year,
+        },
     )
