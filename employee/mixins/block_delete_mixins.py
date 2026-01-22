ae218ad2f523580ac4bb60b2581e0fd2eff58b7e
onongol <rozserbatlin@gmail.com>
2026-01-22 10:28:42 +0800

    Normalize string quoting and improve formatting in block_delete_mixins.py

--- employee/mixins/block_delete_mixins.py
+++ employee/mixins/block_delete_mixins.py
@@ -8,14 +8,15 @@ class BlockDeleteMixin:
     Provides a generic get_block_message based on a list of related record labels.
     Views can set block_related_models = ['Daily Salary', 'Piecework'].
     """
+
     block_related_models: tuple[str, ...] = ()
 
     def get_block_message(self):
         if not self.block_related_models:
-            return ''
-        
-        related = ', '.join(str(_(label)) for label in self.block_related_models)
+            return ""
+
+        related = ", ".join(str(_(label)) for label in self.block_related_models)
         return DELETE_BLOCK_MESSAGE % {
-            'object_name': self.get_object_name(),
-            'related': related
+            "object_name": self.get_object_name(),
+            "related": related,
         }
