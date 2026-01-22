26b1e9e4d8dcf7a974db6ffac0c17568968f843a
onongol <rozserbatlin@gmail.com>
2026-01-22 10:22:14 +0800

    Normalize string quoting in daily_salary_forms.py for consistency

--- employee/forms/daily_salary_forms.py
+++ employee/forms/daily_salary_forms.py
@@ -6,22 +6,19 @@ from employee.models import DailySalary
 class DailySalaryForm(forms.ModelForm):
     class Meta:
         model = DailySalary
-        fields = '__all__'
+        fields = "__all__"
         widgets = {
-            'employee': forms.Select(attrs={'class': 'form-control'}),
-            'hours_per_day': forms.NumberInput(
+            "employee": forms.Select(attrs={"class": "form-control"}),
+            "hours_per_day": forms.NumberInput(
                 attrs={
-                    'class': 'form-control', 
-                    'type': 'number', 
-                    'min': '1',
-                    'max': '24'
+                    "class": "form-control",
+                    "type": "number",
+                    "min": "1",
+                    "max": "24",
                 }
             ),
-            'salary_date': forms.DateInput(
-                attrs={
-                    'class': 'form-control', 
-                    'type': 'date'
-                }
+            "salary_date": forms.DateInput(
+                attrs={"class": "form-control", "type": "date"}
             ),
         }
 
@@ -29,20 +26,17 @@ class DailySalaryForm(forms.ModelForm):
 class UpdateDailySalaryForm(forms.ModelForm):
     class Meta:
         model = DailySalary
-        fields = ['hours_per_day', 'salary_date']
+        fields = ["hours_per_day", "salary_date"]
         widgets = {
-            'hours_per_day': forms.NumberInput(
+            "hours_per_day": forms.NumberInput(
                 attrs={
-                    'class': 'form-control', 
-                    'type': 'number', 
-                    'min': '1',
-                    'max': '24'
+                    "class": "form-control",
+                    "type": "number",
+                    "min": "1",
+                    "max": "24",
                 }
             ),
-            'salary_date': forms.DateInput(
-                attrs={
-                    'class': 'form-control', 
-                    'type': 'date'
-                }
+            "salary_date": forms.DateInput(
+                attrs={"class": "form-control", "type": "date"}
             ),
         }
