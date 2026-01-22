4f4c60b926ac7aa9026cb756c167659d4b1d94f4
onongol <rozserbatlin@gmail.com>
2026-01-22 10:29:30 +0800

    Normalize whitespace in wagon_context_mixins.py

--- employee/mixins/wagon_context_mixins.py
+++ employee/mixins/wagon_context_mixins.py
@@ -4,8 +4,11 @@ from employee.utils.select_department import get_selected_department
 
 class WagonContextMixin:
     """Add SHOW_TYPE_WAGON flag based on department."""
+
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
-        dept = getattr(getattr(self, 'object', None), 'department', None) or get_selected_department(self.request)
-        ctx['SHOW_TYPE_WAGON'] = dept in ALLOWED_WAGON_DEPARTMENTS
+        dept = getattr(
+            getattr(self, "object", None), "department", None
+        ) or get_selected_department(self.request)
+        ctx["SHOW_TYPE_WAGON"] = dept in ALLOWED_WAGON_DEPARTMENTS
         return ctx
