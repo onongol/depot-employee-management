957f2c5b71a1eda36a9ba687a3876fbee70c4851
onongol <rozserbatlin@gmail.com>
2026-01-22 09:14:53 +0800

    Refactor employee_actions.py for consistent string quoting and improved readability

--- employee/views/employee/employee_actions.py
+++ employee/views/employee/employee_actions.py
@@ -1,25 +1,24 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse
-from django.utils.translation import gettext_lazy as _
 
 from employee.models import Employee
 from employee.utils.access import is_admin
 
 
-@login_required(login_url='login')
-@user_passes_test(is_admin, login_url='login')
+@login_required(login_url="login")
+@user_passes_test(is_admin, login_url="login")
 def employee_activate(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     employee.is_active = True
     employee.save()
-    return redirect(reverse('employee_list'))
+    return redirect(reverse("employee_list"))
 
 
-@login_required(login_url='login')
-@user_passes_test(is_admin, login_url='login')
+@login_required(login_url="login")
+@user_passes_test(is_admin, login_url="login")
 def employee_deactivate(request, pk):
     employee = get_object_or_404(Employee, pk=pk)
     employee.is_active = False
     employee.save()
-    return redirect(reverse('employee_list'))
+    return redirect(reverse("employee_list"))
