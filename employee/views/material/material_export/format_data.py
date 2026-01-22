3798814ad70ee66339ffaae39c75d5e1a2bd7bb5
onongol <rozserbatlin@gmail.com>
2026-01-22 09:34:42 +0800

    Refactor format_data.py for consistent string quoting

--- employee/views/material/material_export/format_data.py
+++ employee/views/material/material_export/format_data.py
@@ -2,9 +2,9 @@ def iter_rows(qs):
     for i, item in enumerate(qs, start=1):
         row = [
             i,
-            item['work__type_material'] or "",
-            item['work__work_name'] or "",
-            item['amount_material'] or 0,
-            item['work_date'] or "",
+            item["work__type_material"] or "",
+            item["work__work_name"] or "",
+            item["amount_material"] or 0,
+            item["work_date"] or "",
         ]
         yield row
