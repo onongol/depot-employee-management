e5b8a4f42c6230155bd11c064ca38625dbcd31e6
onongol <rozserbatlin@gmail.com>
2026-01-22 10:29:12 +0800

    Normalize string quoting in department_mixins.py

--- employee/mixins/department_mixins.py
+++ employee/mixins/department_mixins.py
@@ -3,8 +3,8 @@ from employee.utils.select_department import get_selected_department
 
 class InitialDepartmentMixin:
     """Populate 'department' in form initial from user selection."""
+
     def get_initial(self):
         initial = super().get_initial()
-        initial['department'] = get_selected_department(self.request)
+        initial["department"] = get_selected_department(self.request)
         return initial
-    
