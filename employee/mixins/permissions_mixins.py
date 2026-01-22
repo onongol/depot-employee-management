af30a99b68ab916b84e402cce6d7bba8d0f61416
onongol <rozserbatlin@gmail.com>
2026-01-22 10:29:24 +0800

    Normalize whitespace in permissions_mixins.py

--- employee/mixins/permissions_mixins.py
+++ employee/mixins/permissions_mixins.py
@@ -5,18 +5,23 @@ from employee.constants.constants import GroupNames
 
 class OnlyGroupMixin(UserPassesTestMixin):
     """Mixin to restrict access to users in specific groups or superusers."""
+
     group_names = []
 
     def test_func(self):
         user = self.request.user
-        return user.is_superuser or user.groups.filter(name__in=self.group_names).exists()
+        return (
+            user.is_superuser or user.groups.filter(name__in=self.group_names).exists()
+        )
 
 
 class OnlyAdminMixin(OnlyGroupMixin):
     """Mixin to restrict access to users in Payrolls group or superusers."""
+
     group_names = [GroupNames.PAYROLLS.value]
 
 
 class OnlyCreaterMixin(OnlyGroupMixin):
     """Mixin to restrict access to users in Payrolls or Masters groups or superusers."""
+
     group_names = [GroupNames.PAYROLLS.value, GroupNames.MASTERS.value]
