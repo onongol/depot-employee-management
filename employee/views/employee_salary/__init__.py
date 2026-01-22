4e65423ed3411bae9912b117e3bfce6651531b10
onongol <rozserbatlin@gmail.com>
2026-01-22 09:25:18 +0800

    Reorder import statements in __init__.py for improved clarity

--- employee/views/employee_salary/__init__.py
+++ employee/views/employee_salary/__init__.py
@@ -1,3 +1,3 @@
-from .employee_salary_views import *
 from .employee_salary_export.employee_salary_excel import *
 from .employee_salary_export.employee_salary_pdf import *
+from .employee_salary_views import *
