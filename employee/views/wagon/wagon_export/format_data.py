0c872fcd42ce2cdb8eb9a4157f0f7eaa5e48244c
onongol <rozserbatlin@gmail.com>
2026-01-22 09:57:31 +0800

    Refactor iter_rows function for consistent string quoting

--- employee/views/wagon/wagon_export/format_data.py
+++ employee/views/wagon/wagon_export/format_data.py
@@ -6,23 +6,23 @@ def iter_rows(qs, group=None):
         if group == GROUP_MONTH:
             row = [
                 i,
-                item.get('wagon_number') or "",
-                item.get('type_wagon') or "",
-                item.get('total_time') or 0,
-                item.get('total_price') or 0,
-                item.get('month') or "",
-                item.get('year') or "",
+                item.get("wagon_number") or "",
+                item.get("type_wagon") or "",
+                item.get("total_time") or 0,
+                item.get("total_price") or 0,
+                item.get("month") or "",
+                item.get("year") or "",
             ]
         else:
             row = [
                 i,
-                item.get('wagon_number') or "",
-                item.get('type_wagon') or "",
-                item.get('work__work_name') or "",
-                item.get('type_work') or "",
-                item.get('amount') or 0,
-                item.get('total_time') or 0,
-                item.get('total_price') or 0,
-                item.get('work_date') or "",
+                item.get("wagon_number") or "",
+                item.get("type_wagon") or "",
+                item.get("work__work_name") or "",
+                item.get("type_work") or "",
+                item.get("amount") or 0,
+                item.get("total_time") or 0,
+                item.get("total_price") or 0,
+                item.get("work_date") or "",
             ]
         yield row
