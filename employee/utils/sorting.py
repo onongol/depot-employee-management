11e8b87b50e2b8609dc46932d1fe7858c223ce6a
onongol <rozserbatlin@gmail.com>
2026-01-22 10:49:34 +0800

    Refactor apply_ordering function for consistent string formatting

--- employee/utils/sorting.py
+++ employee/utils/sorting.py
@@ -1,4 +1,6 @@
-def apply_ordering(queryset, order_by, direction, allowed_fields, default='-record_date'):
+def apply_ordering(
+    queryset, order_by, direction, allowed_fields, default="-record_date"
+):
     """
     Applies ordering to a queryset based on the specified field and direction.
     :param queryset: QuerySet to be ordered
@@ -9,11 +11,11 @@ def apply_ordering(queryset, order_by, direction, allowed_fields, default='-reco
     :return: ordered QuerySet
     """
     if order_by in allowed_fields:
-        sort_field = f"-{order_by}" if direction == 'desc' else order_by
+        sort_field = f"-{order_by}" if direction == "desc" else order_by
         return queryset.order_by(sort_field)
-    
+
     # Support default as string or iterable of fields
     if isinstance(default, (list, tuple)):
         return queryset.order_by(*default)
-    
+
     return queryset.order_by(default)
