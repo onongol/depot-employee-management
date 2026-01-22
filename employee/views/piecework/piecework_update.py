01e19f0c0affbc857f5a1ce366420c4b08dd1dd7
onongol <rozserbatlin@gmail.com>
2026-01-22 09:48:51 +0800

    Refactor PieceworkUpdateView for improved readability and consistency in formatting

--- employee/views/piecework/piecework_update.py
+++ employee/views/piecework/piecework_update.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect
-from django.utils.translation import gettext_lazy as _
 from django.views.generic import UpdateView
 
 from employee.forms import UpdatePieceworkForm
@@ -8,45 +7,55 @@ from employee.mixins.context_mixins import PieceworkContextMixin
 from employee.mixins.permissions_mixins import OnlyAdminMixin
 from employee.models import DailySalary
 from employee.utils.select_department import get_selected_department
-from employee.views.piecework.piecework_calculation import piecework_calculate_update
+from employee.views.piecework.calculation.piecework_calculation_update import (
+    piecework_calculate_update,
+)
 
 
-class PieceworkUpdateView(LoginRequiredMixin, OnlyAdminMixin, PieceworkContextMixin, UpdateView):
-    login_url = 'login'
+class PieceworkUpdateView(
+    LoginRequiredMixin, OnlyAdminMixin, PieceworkContextMixin, UpdateView
+):
+    login_url = "login"
     form_class = UpdatePieceworkForm
-    template_name = 'piecework/piecework_update.html'
+    template_name = "piecework/piecework_update.html"
 
     def get_form_kwargs(self):
         """Add department to form kwargs for filtering."""
         kwargs = super().get_form_kwargs()
         department = get_selected_department(self.request)
-        kwargs['department'] = department
+        kwargs["department"] = department
         return kwargs
 
     def form_valid(self, form):
         """Handle form validation and calculate amount price based on daily salary."""
         piecework = form.instance
-        amount = form.cleaned_data.get('amount')
+        amount = form.cleaned_data.get("amount")
         work = piecework.work
         work_date = piecework.work_date
         employee = piecework.employee
         department = get_selected_department(self.request)
 
-        wagon_number = form.cleaned_data.get('wagon_number')
+        wagon_number = form.cleaned_data.get("wagon_number")
         if not wagon_number:
             piecework.wagon_number = None
 
         # Get the daily salary for the employee on the work date
-        daily_salary = DailySalary.objects.filter(employee=employee, salary_date=work_date).first()
+        daily_salary = DailySalary.objects.filter(
+            employee=employee, salary_date=work_date
+        ).first()
 
         # Get all daily salaries for the department on the work date
-        employees_salary = DailySalary.objects.filter(employee__department=department, salary_date=work_date)
+        employees_salary = DailySalary.objects.filter(
+            employee__department=department, salary_date=work_date
+        )
 
         # Calculate amount_price using business logic function
-        amount_price = piecework_calculate_update(work, amount, daily_salary, employees_salary)
+        amount_price = piecework_calculate_update(
+            work, amount, daily_salary, employees_salary
+        )
 
         piecework.amount_price = amount_price
 
         form.save()
 
-        return redirect('piecework_list')
+        return redirect("piecework_list")
