52fe0692a2fa81c16969134ee17f6cf7a6e073bb
onongol <rozserbatlin@gmail.com>
2026-01-22 09:46:54 +0800

    Add group_pieceworks_by_year function to aggregate pieceworks by year

--- employee/views/piecework/group/group_by_year.py
+++ employee/views/piecework/group/group_by_year.py
@@ -10,17 +10,21 @@ def group_pieceworks_by_year(qs, *, show_wagon: bool):
         year=ExtractYear("work_date"),
     )
 
-    group_fields = ["employee_id", "employee_name", "job_title", "work_name", "type_work", "year"]
+    group_fields = [
+        "employee_id",
+        "employee_name",
+        "job_title",
+        "work_name",
+        "type_work",
+        "year",
+    ]
 
     if show_wagon:
         group_fields.insert(5, "type_wagon")
         group_fields.insert(6, "wagon_number")
-        
-    return (
-        qs.values(*group_fields)
-        .annotate(
-            total_amount=Sum("amount"),
-            total_time=Sum("amount_time"),
-            total_price=Sum("amount_price"),
-        )
+
+    return qs.values(*group_fields).annotate(
+        total_amount=Sum("amount"),
+        total_time=Sum("amount_time"),
+        total_price=Sum("amount_price"),
     )
