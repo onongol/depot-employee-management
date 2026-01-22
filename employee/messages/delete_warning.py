42c9a25c40fa9cc4a358cfe390cc70200f6bb98a
onongol <rozserbatlin@gmail.com>
2026-01-22 10:26:22 +0800

    Normalize string quoting and improve formatting in delete_warning.py

--- employee/messages/delete_warning.py
+++ employee/messages/delete_warning.py
@@ -1,12 +1,13 @@
 from django.contrib import messages
-from django.utils.translation import gettext_lazy as _ 
+from django.utils.translation import gettext_lazy as _
 
 
 def send_delete_warning(request, object_name):
     """Send a warning message after deleting an object."""
     messages.warning(
         request,
-        _('You have deleted %(object_name)s. Also, all related records have been deleted.') % {
-            'object_name': object_name
-        }
+        _(
+            "You have deleted %(object_name)s. Also, all related records have been deleted."
+        )
+        % {"object_name": object_name},
     )
