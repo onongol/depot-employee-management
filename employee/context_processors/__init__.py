efcd78e3b904d9d33274d193718021333b25f8da
onongol <rozserbatlin@gmail.com>
2026-01-22 10:19:42 +0800

    Normalize string quoting in __init__.py for consistency

--- employee/context_processors/__init__.py
+++ employee/context_processors/__init__.py
@@ -1,12 +1,11 @@
-from .departments import global_departments
 from .department_warning import needs_department_warning
+from .departments import global_departments
 from .permissions import is_employee, is_master, is_payroll
 
-
 __all__ = [
-    'global_departments',
-    'needs_department_warning',
-    'is_employee',
-    'is_master',
-    'is_payroll',
+    "global_departments",
+    "needs_department_warning",
+    "is_employee",
+    "is_master",
+    "is_payroll",
 ]
