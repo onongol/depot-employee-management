74e7a26e0c55fbf91a53e80c2f75121e2f13e662
onongol <rozserbatlin@gmail.com>
2026-01-22 10:29:17 +0800

    Normalize whitespace and string quoting in generic_mixins.py

--- employee/mixins/generic_mixins.py
+++ employee/mixins/generic_mixins.py
@@ -1,30 +1,34 @@
 class GenericContextMixin:
     """Generic mixin to add object name, type, department, and cancel URL to context."""
+
     model = None
     object_type = None
-    object_name_func = None  # Should be a staticmethod or classmethod
+    object_name_func = None
     success_url = None
     cancel_url = None
 
     def get_context_data(self, **kwargs):
-        """Add object name, type, department, and cancel URL to context."""        
+        """Add object name, type, department, and cancel URL to context."""
         context = super().get_context_data(**kwargs)
-        obj = getattr(self, 'object', None)
-        
-        # Get selected department from request GET parameters or session
-        department = self.request.GET.get('department') or self.request.session.get('department')
+        obj = getattr(self, "object", None)
 
-        context['selected_department'] = department
+        # Get selected department from request GET parameters or session
+        department = self.request.GET.get("department") or self.request.session.get(
+            "department"
+        )
+
+        context["selected_department"] = department
 
         # Get object name using the provided function or str()
         if obj:
             if self.object_name_func:
-                context['object_name'] = self.object_name_func(obj)
+                context["object_name"] = self.object_name_func(obj)
             else:
-                context['object_name'] = str(obj)
+                context["object_name"] = str(obj)
         else:
-            context['object_name'] = ''
+            context["object_name"] = ""
 
-        context['object_type'] = self.object_type
-        context['cancel_url'] = self.cancel_url
+        context["object_type"] = self.object_type
+        context["cancel_url"] = self.cancel_url
+        
         return context
