b87efab2952b147d9529f2ce79e31f2e0b34188e
onongol <rozserbatlin@gmail.com>
2026-01-22 10:22:48 +0800

    Normalize string quoting in piecework_forms.py for consistency

--- employee/forms/piecework_forms.py
+++ employee/forms/piecework_forms.py
@@ -7,40 +7,30 @@ from employee.models import Piecework
 class PieceworkForm(TypeWorkChoiceMixin, forms.ModelForm):
     class Meta:
         model = Piecework
-        fields = '__all__'
+        fields = "__all__"
         widgets = {
-            'employee': forms.Select(attrs={'class': 'form-control'}),
-            'work': forms.Select(attrs={'class': 'form-control'}),
-            'type_work': forms.Select(attrs={'class': 'form-control'}),
-            'work_date': forms.DateInput(attrs={
-                'class': 'form-control', 
-                'type': 'date'
-                }
+            "employee": forms.Select(attrs={"class": "form-control"}),
+            "work": forms.Select(attrs={"class": "form-control"}),
+            "type_work": forms.Select(attrs={"class": "form-control"}),
+            "work_date": forms.DateInput(
+                attrs={"class": "form-control", "type": "date"}
             ),
-            'amount': forms.NumberInput(attrs={
-                'class': 'form-control', 
-                'type': 'number', 
-                'min': '0.01'
-                }
+            "amount": forms.NumberInput(
+                attrs={"class": "form-control", "type": "number", "min": "0.01"}
             ),
-        }        
-        
+        }
+
 
 class UpdatePieceworkForm(TypeWorkChoiceMixin, forms.ModelForm):
     class Meta:
         model = Piecework
-        fields = ['type_work', 'wagon_number', 'amount', 'work_date']
+        fields = ["type_work", "wagon_number", "amount", "work_date"]
         widgets = {
-            'type_work': forms.Select(attrs={'class': 'form-control'}),
-            'work_date': forms.DateInput(attrs={
-                'class': 'form-control', 
-                'type': 'date'
-                }
+            "type_work": forms.Select(attrs={"class": "form-control"}),
+            "work_date": forms.DateInput(
+                attrs={"class": "form-control", "type": "date"}
             ),
-            'amount': forms.NumberInput(attrs={
-                'class': 'form-control', 
-                'type': 'number', 
-                'min': '0.01'
-                }
-            ),  
+            "amount": forms.NumberInput(
+                attrs={"class": "form-control", "type": "number", "min": "0.01"}
+            ),
         }
