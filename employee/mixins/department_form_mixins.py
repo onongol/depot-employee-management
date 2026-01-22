0d569db1cab0ee3f502e1a69fdbdda3949d59269
onongol <rozserbatlin@gmail.com>
2026-01-22 10:29:03 +0800

    Normalize string quoting in department_form_mixins.py

--- employee/mixins/department_form_mixins.py
+++ employee/mixins/department_form_mixins.py
@@ -3,7 +3,8 @@ from employee.utils.select_department import get_selected_department
 
 class FormDepartmentMixin:
     """Inject selected department into form kwargs."""
+
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
-        kwargs['department'] = get_selected_department(self.request)
+        kwargs["department"] = get_selected_department(self.request)
         return kwargs
