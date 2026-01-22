50d3b5db779b79553dbffd8ea8bef9807e1fe577
onongol <rozserbatlin@gmail.com>
2026-01-22 09:09:23 +0800

    Refactor validate_duplicate.py for improved code readability and consistency

--- employee/views/daily_work/validators/validate_duplicate.py
+++ employee/views/daily_work/validators/validate_duplicate.py
@@ -4,11 +4,18 @@ from employee.models import Piecework
 from employee.services.normalizes import normalize_wagon_number
 
 
-def validate_duplicate(selected_employee_ids, selected_work_ids, work_date, type_work, wagon_number):
+def validate_duplicate(
+    selected_employee_ids, selected_work_ids, work_date, type_work, wagon_number
+):
     """Validate that no duplicate Piecework entries exist for the given parameters."""
     errors = []
 
-    if not selected_employee_ids or not selected_work_ids or not work_date or not type_work:
+    if (
+        not selected_employee_ids
+        or not selected_work_ids
+        or not work_date
+        or not type_work
+    ):
         return errors
 
     wagon_number = normalize_wagon_number(wagon_number)
@@ -17,7 +24,6 @@ def validate_duplicate(selected_employee_ids, selected_work_ids, work_date, type
     qs = Piecework.objects.filter(
         employee__employee_id__in=selected_employee_ids,
         work_id__in=selected_work_ids,
-
         type_work=type_work,
         work_date=work_date,
     )
