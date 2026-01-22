576c87a390c334caf83700d5b01c06e9699e4c1e
onongol <rozserbatlin@gmail.com>
2026-01-22 09:31:03 +0800

    Refactor home_views.py for improved formatting and readability

--- employee/views/home/home_views.py
+++ employee/views/home/home_views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
 
 
-def home(request):
-    """View to render the home page."""
-    department = request.GET.get('department') or request.session.get('department')
+def home(request):    
+    department = request.GET.get("department") or request.session.get("department")
 
-    return render(
-        request, 
-        'home/home.html',
-        {
-            'selected_department': department
-        }
-    )
+    return render(request, "home/home.html", {"selected_department": department})
