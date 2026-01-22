f071bda5d026ccddf622d3c2bc2b56762608d2ba
onongol <rozserbatlin@gmail.com>
2026-01-22 09:29:51 +0800

    Refactor employee_salary_excel.py for improved formatting and readability

--- employee/views/employee_salary/employee_salary_export/employee_salary_excel.py
+++ employee/views/employee_salary/employee_salary_export/employee_salary_excel.py
@@ -2,9 +2,13 @@ from django.utils.translation import gettext_lazy as _
 
 from employee.constants.constants import GROUP_WAGON
 from employee.utils.exports.export_excel import export_to_excel
-from employee.views.employee_salary.employee_salary_export.build_headers import build_headers
+from employee.views.employee_salary.employee_salary_export.build_headers import (
+    build_headers,
+)
+from employee.views.employee_salary.employee_salary_export.employee_salary_service import (
+    get_employee_salaries,
+)
 from employee.views.employee_salary.employee_salary_export.format_data import iter_rows
-from employee.views.employee_salary.employee_salary_export.employee_salary_service import get_employee_salaries
 
 
 def employee_salary_export_excel(request):
@@ -18,7 +22,9 @@ def employee_salary_export_excel(request):
         GROUP_WAGON: ("employee_salaries_wagon.xlsx", _("Employee Salaries by Wagon")),
     }
 
-    file_name, title = meta.get(group, ("employee_salaries.xlsx", _("Employee Salaries")))
+    file_name, title = meta.get(
+        group, ("employee_salaries.xlsx", _("Employee Salaries"))
+    )
     sheet_title = str(title)
 
     return export_to_excel(data, headers, file_name, sheet_title)
