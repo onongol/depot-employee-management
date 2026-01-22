bcb710c6c4777170810edcba2159db0273d398ed
onongol <rozserbatlin@gmail.com>
2026-01-22 09:30:16 +0800

    Refactor employee_salary_service.py for improved formatting and readability

--- employee/views/employee_salary/employee_salary_export/employee_salary_service.py
+++ employee/views/employee_salary/employee_salary_export/employee_salary_service.py
@@ -1,40 +1,44 @@
 from employee.utils.filters import filter_employees
-from employee.views.employee_salary.employee_salary_calculate import employee_salary_calculate
-from employee.views.employee_salary.employee_salary_prepare import employee_salaries_prepare
+from employee.views.employee_salary.employee_salary_calculate import (
+    employee_salary_calculate,
+)
+from employee.views.employee_salary.employee_salary_prepare import (
+    employee_salaries_prepare,
+)
 from employee.views.employee_salary.employee_salary_sort import apply_ordering
 
 
 def get_employee_salaries(request):
     (
-        employees, 
-        employee_id, 
-        employee_name, 
-        department, 
+        employees,
+        employee_id,
+        employee_name,
+        department,
         job_title,
-        wagon_number, 
-        month, 
-        year, 
-        month_period, 
-        group, 
-        order_by, 
+        wagon_number,
+        month,
+        year,
+        month_period,
+        group,
+        order_by,
         direction,
-        wagon_mode
+        wagon_mode,
     ) = employee_salaries_prepare(request)
 
     employees = filter_employees(
-        employees, 
-        department=department, 
-        employee_id=employee_id, 
-        employee_name=employee_name, 
-        job_title=job_title
+        employees,
+        department=department,
+        employee_id=employee_id,
+        employee_name=employee_name,
+        job_title=job_title,
     )
-   
+
     employee_salaries = employee_salary_calculate(
-        employees, 
-        month, 
-        year, 
+        employees,
+        month,
+        year,
         group_by_wagon=wagon_mode,
-        wagon_number=wagon_number if wagon_mode else None
+        wagon_number=wagon_number if wagon_mode else None,
     )
 
     apply_ordering(
