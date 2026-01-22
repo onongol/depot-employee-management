957f440492afefa1c83125a94894c03280c0fa69
onongol <rozserbatlin@gmail.com>
2026-01-22 10:25:26 +0800

    Normalize string quoting in type_work_mixins.py

--- employee/forms/forms_mixins/type_work_mixins.py
+++ employee/forms/forms_mixins/type_work_mixins.py
@@ -3,8 +3,9 @@ from employee.constants.constants import get_type_work_choices
 
 class TypeWorkChoiceMixin:
     """Mixin to set type work choices based on department."""
+
     def __init__(self, *args, **kwargs):
-        department = kwargs.pop('department', None)
+        department = kwargs.pop("department", None)
         super().__init__(*args, **kwargs)
-        if 'type_work' in self.fields:
-            self.fields['type_work'].choices = get_type_work_choices(department)
+        if "type_work" in self.fields:
+            self.fields["type_work"].choices = get_type_work_choices(department)
