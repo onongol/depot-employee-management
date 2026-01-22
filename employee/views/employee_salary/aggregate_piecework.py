f8b26157fd467f83c18c37b0cf9ed4ea4d767022
onongol <rozserbatlin@gmail.com>
2026-01-22 09:25:26 +0800

    Refactor aggregate_piecework.py for consistent string quoting and improved readability

--- employee/views/employee_salary/aggregate_piecework.py
+++ employee/views/employee_salary/aggregate_piecework.py
@@ -2,15 +2,13 @@ from django.db.models import QuerySet, Sum
 
 
 def aggregate_piecework(qs: QuerySet, *, group_by_wagon: bool):
-    '''Aggregate Piecework in the database (SUM) by employee and month; optionally split totals by wagon_number.'''
+    """Aggregate Piecework in the database (SUM) by employee and month; optionally split totals by wagon_number."""
     values_fields = ["employee", "work_date__year", "work_date__month"]
 
     if group_by_wagon:
         values_fields.append("wagon_number")
 
-    return (
-        qs.values(*values_fields).annotate(
-            total_piecework_amount=Sum("amount_price"),
-            total_piecework_time=Sum("amount_time"),
-        )
+    return qs.values(*values_fields).annotate(
+        total_piecework_amount=Sum("amount_price"),
+        total_piecework_time=Sum("amount_time"),
     )
