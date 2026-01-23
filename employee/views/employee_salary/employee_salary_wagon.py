fe02d7a716a72805d8e7ebbf2a360e6f1c3f21cf
onongol <rozserbatlin@gmail.com>
2026-01-23 11:38:11 +0800

    Remove unused is_wagon_group function and related constants from employee_salary_wagon.py

--- employee/views/employee_salary/employee_salary_wagon.py
+++
@@ -1,7 +0,0 @@
-from employee.constants.constants import ALLOWED_WAGON_DEPARTMENTS, GROUP_WAGON
-
-
-def is_wagon_group(*, department: str | None, group: str | None) -> bool:
-    """Checks if wagon grouping is active for department and request."""
-    
-    return (group == GROUP_WAGON) and (department in ALLOWED_WAGON_DEPARTMENTS)
