25abd5988b8e361164c084e657b49864988a9781
onongol <rozserbatlin@gmail.com>
2026-01-26 10:20:42 +0800

    Refactor piecework_export_excel to streamline parameter handling using context object

--- employee/views/piecework/piecework_export/piecework_excel.py
+++ employee/views/piecework/piecework_export/piecework_excel.py
@@ -12,74 +12,26 @@ from employee.views.piecework.piecework_prepare import piecework_prepare
 
 def piecework_export_excel(request):
     """Export filtered Piecework queryset to Excel."""
-    # Prepare data
-    (
-        pieceworks,
-        department,
-        employee_id,
-        employee_name,
-        job_title,
-        work_name,
-        type_work,
-        wagon_number,
-        type_wagon,
-        type_material,
-        range_date,
-        record_date,
-        group,
-        selected_year,
-        month,
-        year,
-        month_period,
-        order_by,
-        direction,
-        show_wagon,
-        detail_group,
-        month_group,
-        year_group,
-    ) = piecework_prepare(request)
+    pw_context = piecework_prepare(request)
 
-    # Apply filters
-    pieceworks = filter_pieceworks(
-        pieceworks,
-        employee_id=employee_id,
-        employee_name=employee_name,
-        job_title=job_title,
-        work_name=work_name,
-        type_work=type_work,
-        wagon_number=wagon_number,
-        type_wagon=type_wagon,
-        type_material=type_material,
-        range_date=range_date,
-        record_date=record_date,
-    )
+    pieceworks = pw_context.pieceworks
 
-    pieceworks = group_and_sort_pieceworks(
-        pieceworks,
-        group=group,
-        month=month,
-        year=year,
-        selected_year=selected_year,
-        show_wagon=show_wagon,
-        order_by=order_by,
-        direction=direction,
-    )
+    pieceworks = filter_pieceworks(pieceworks, context=pw_context)
 
-    # Build headers
-    headers = build_headers(department, group=group)
+    pieceworks = group_and_sort_pieceworks(pieceworks, context=pw_context)
 
-    # Format data for Excel
-    data = list(iter_rows(pieceworks, department, group=group))
+    headers = build_headers(context=pw_context)
 
-    # Append totals row
-    data.append(build_totals_row(pieceworks, department, group=group))
+    data = list(iter_rows(pieceworks, context=pw_context))
+
+    data.append(build_totals_row(pieceworks, context=pw_context))
 
     meta = {
         GROUP_MONTH: ("monthly_piecework.xlsx", _("Monthly Piecework")),
         GROUP_YEAR: ("yearly_piecework.xlsx", _("Yearly Piecework")),
     }
 
-    file_name, title = meta.get(group, ("piecework.xlsx", _("Piecework")))
+    file_name, title = meta.get(pw_context.group, ("piecework.xlsx", _("Piecework")))
     sheet_title = str(title)
 
     return export_to_excel(data, headers, file_name, sheet_title)
