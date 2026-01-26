c68a22ac3c6704ca7a51b784927e3d7500950880
onongol <rozserbatlin@gmail.com>
2026-01-26 10:42:54 +0800

    Refactor daily_work_export_excel to streamline parameter handling using context object

--- employee/views/daily_work/daily_work_export/daily_work_export_excel.py
+++ employee/views/daily_work/daily_work_export/daily_work_export_excel.py
@@ -17,42 +17,27 @@ def daily_work_export_excel(request):
     dw_context = daily_work_prepare(request)
 
     daily_works = dw_context.daily_works
-    show_wagon = dw_context.show_wagon
-    month_group = dw_context.month_group
-    year_group = dw_context.year_group
+    department = dw_context.selected_department
+
+    safe_department = (department or "all").replace(" ", "_")
 
     daily_works = filter_daily_works(daily_works, context=dw_context)
 
     daily_works = group_and_sort_daily_works(daily_works, context=dw_context)
 
-    headers = build_headers(
-        show_wagon=show_wagon, month_group=month_group, year_group=year_group
-    )
+    headers = build_headers(context=dw_context)
 
-    data = list(
-        iter_rows(
-            daily_works,
-            show_wagon=show_wagon,
-            month_group=month_group,
-            year_group=year_group,
-        )
-    )
+    data = list(iter_rows(daily_works, context=dw_context))
 
-    data.append(
-        build_totals_row(
-            daily_works,
-            show_wagon=show_wagon,
-            month_group=month_group,
-            year_group=year_group,
-        )
-    )
+    data.append(build_totals_row(daily_works, context=dw_context))
 
     meta = {
-        GROUP_MONTH: ("monthly_work.xlsx", _("Monthly Work")),
-        GROUP_YEAR: ("yearly_work.xlsx", _("Yearly Work")),
+        GROUP_MONTH: ("monthly_work", _("Monthly Work")),
+        GROUP_YEAR: ("yearly_work", _("Yearly Work")),
     }
 
-    file_name, title = meta.get(dw_context.group, ("daily_work.xlsx", _("Daily Work")))
-    sheet_title = str(title)
+    file_name, title = meta.get(dw_context.group, ("daily_work", _("Daily Work")))
+    file_name = f"{file_name}_{safe_department}.xlsx"
+    sheet_title = f"{title} ({department})"
 
     return export_to_excel(data, headers, file_name, sheet_title)
