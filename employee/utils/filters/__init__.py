568c258156348327544b6d43f0f977d00c9f8f82
onongol <rozserbatlin@gmail.com>
2026-01-26 15:49:04 +0800

    Add filter_employees_s to filters and __all__ exports

--- employee/utils/filters/__init__.py
+++ employee/utils/filters/__init__.py
@@ -1,6 +1,7 @@
 from .filter_daily_salaries import filter_daily_salaries
 from .filter_daily_works import filter_daily_works
 from .filter_employees import filter_employees
+from .filter_employees_s import filter_employees_s 
 from .filter_material import filter_material
 from .filter_month_year import filter_month_year
 from .filter_pieceworks import filter_pieceworks
@@ -9,6 +10,7 @@ from .filter_works import filter_works
 
 __all__ = [
     "filter_employees",
+    "filter_employees_s",
     "filter_works",
     "filter_daily_salaries",
     "filter_daily_works",
