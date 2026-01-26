5e5c38fe8c49d8e260a9cb8246e6a0fc1ba325b2
onongol <rozserbatlin@gmail.com>
2026-01-26 15:52:28 +0800

    Rename filter_employees_s to filter_employees_salary for consistency in filter exports

--- employee/utils/filters/__init__.py
+++ employee/utils/filters/__init__.py
@@ -1,7 +1,7 @@
 from .filter_daily_salaries import filter_daily_salaries
 from .filter_daily_works import filter_daily_works
 from .filter_employees import filter_employees
-from .filter_employees_s import filter_employees_s 
+from .filter_employees_salary import filter_employees_salary
 from .filter_material import filter_material
 from .filter_month_year import filter_month_year
 from .filter_pieceworks import filter_pieceworks
@@ -10,7 +10,7 @@ from .filter_works import filter_works
 
 __all__ = [
     "filter_employees",
-    "filter_employees_s",
+    "filter_employees_salary",
     "filter_works",
     "filter_daily_salaries",
     "filter_daily_works",
