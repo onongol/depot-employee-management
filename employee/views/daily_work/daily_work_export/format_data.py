b368f1f67aa2d34b57d3c7d272b48c4957826f27
onongol <rozserbatlin@gmail.com>
2026-01-26 08:48:11 +0800

    Refactor iter_rows to simplify parameters and remove unused dependencies

--- employee/views/daily_work/daily_work_export/format_data.py
+++ employee/views/daily_work/daily_work_export/format_data.py
@@ -1,13 +1,9 @@
 from employee.utils.get_value import get_value
-from employee.utils.group_modes import is_grouped, is_month_group
-from employee.utils.wagon_department import is_wagon_department
 
 
-def iter_rows(qs, department, group=None):
+def iter_rows(qs, show_wagon=False, month_group=None, year_group=None):
     """Generate rows for DailyWork export (detailed or grouped)."""
-    show_wagon = is_wagon_department(department)
-    grouped = is_grouped(group)
-    month_group = is_month_group(group)
+    grouped = month_group or year_group
 
     for i, dw in enumerate(qs, start=1):
         work_name = get_value(dw, "work_name", "") or ""
