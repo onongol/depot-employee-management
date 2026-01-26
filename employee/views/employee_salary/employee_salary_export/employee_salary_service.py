251660e2bf9711a08d5ba23335b1c2680cc78698
onongol <rozserbatlin@gmail.com>
2026-01-26 15:53:12 +0800

    Refactor get_employee_salaries to use filter_employees_salary and streamline context handling

--- employee/views/employee_salary/employee_salary_export/employee_salary_service.py
+++ employee/views/employee_salary/employee_salary_export/employee_salary_service.py
@@ -1,4 +1,4 @@
-from employee.utils.filters import filter_employees
+from employee.utils.filters import filter_employees_salary
 from employee.views.employee_salary.employee_salary_calculate import (
     employee_salary_calculate,
 )
@@ -9,45 +9,25 @@ from employee.views.employee_salary.employee_salary_sort import apply_ordering
 
 
 def get_employee_salaries(request):
-    (
-        employees,
-        employee_id,
-        employee_name,
-        department,
-        job_title,
-        wagon_number,
-        month,
-        year,
-        month_period,
-        group,
-        order_by,
-        direction,
-        show_wagon,
-        wagon_group,
-        wagon_mode,
-    ) = employee_salaries_prepare(request)
+    es_context = employee_salaries_prepare(request)
 
-    employees = filter_employees(
+    employees = es_context.employees
+
+    employees = filter_employees_salary(
         employees,
-        department=department,
-        employee_id=employee_id,
-        employee_name=employee_name,
-        job_title=job_title,
+        context=es_context
     )
 
     employee_salaries = employee_salary_calculate(
         employees,
-        month,
-        year,
-        group_by_wagon=wagon_mode,
-        wagon_number=wagon_number if wagon_mode else None,
+        context=es_context,
+        wagon_number=es_context.wagon_number if es_context.wagon_mode else None,
     )
 
     apply_ordering(
         employee_salaries,
-        order_by,
-        direction,
+        context=es_context,
         allowed_fields=["employee_id", "month", "year"],
     )
 
-    return employee_salaries, group, wagon_mode
+    return employee_salaries, es_context
