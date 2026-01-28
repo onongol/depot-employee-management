54fc55602b74e548aa161d97fcab66a6b314ac36
onongol <rozserbatlin@gmail.com>
2026-01-28 15:27:09 +0800

    Fix import path for piecework calculation in daily work synchronization

--- employee/services/daily_work_sync.py
+++ employee/services/daily_work_sync.py
@@ -20,7 +20,7 @@ def sync_piecework_with_dailywork(dailywork):
     try:
         # Local imports to avoid circular import issues
         from employee.models import DailySalary, Piecework
-        from employee.views.piecework.calculation.piecework_calculation import (
+        from employee.views.daily_work.daily_work_create.calculation.piecework_calculation import (
             piecework_calculate_update,
         )
 
