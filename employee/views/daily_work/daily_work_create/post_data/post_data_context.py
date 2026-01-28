274e179695a84fbbe1d1a54b350777b155061302
onongol <rozserbatlin@gmail.com>
2026-01-28 15:28:55 +0800

    Add PostData dataclass to structure daily work entry data

---
+++ employee/views/daily_work/daily_work_create/post_data/post_data_context.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import Dict, List, Optional
+
+
+@dataclass
+class PostData:
+    work_date: Optional[str]
+    type_work: Optional[str]
+    job_title: Optional[str]
+    wagon_number: Optional[str]
+    selected_employee_ids: List[str]
+    selected_work_ids: List[str]
+    amounts: Dict[str, str]
+    
