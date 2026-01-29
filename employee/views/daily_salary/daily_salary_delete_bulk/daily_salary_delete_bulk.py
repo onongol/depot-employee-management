834b1f4b01c8e978f1dadd1fa2202b65813c65c9
onongol <rozserbatlin@gmail.com>
2026-01-29 11:20:41 +0800

    Refactor daily_salary_delete_bulk view to update import paths and improve messaging functions

--- employee/views/daily_salary/daily_salary_delete_bulk/daily_salary_delete_bulk.py
+++ employee/views/daily_salary/daily_salary_delete_bulk/daily_salary_delete_bulk.py
@@ -8,17 +8,17 @@ from django.views.decorators.http import require_POST
 from employee.utils.access import is_admin
 from employee.utils.parse_ids import parse_ids
 from employee.utils.select_department import get_selected_department
-from employee.views.daily_salary.daily_salary_delete_bulk.bulk_queryset_split import (
-    get_deletable_and_blocked_qs,
+from employee.views.daily_salary.daily_salary_delete_bulk.daily_salary_selectors import (
+    get_deletable_and_blocked_daily_salaries,
 )
-from employee.views.daily_salary.daily_salary_delete_bulk.messages.block_bulk_messages import (
-    block_bulk_messages,
+from employee.views.daily_salary.daily_salary_delete_bulk.messages.blocked_bulk_message import (
+    blocked_bulk_message,
 )
-from employee.views.daily_salary.daily_salary_delete_bulk.messages.delete_bulk_messanges import (
-    delete_bulk_messages,
+from employee.views.daily_salary.daily_salary_delete_bulk.messages.bulk_preview_helpers import (
+    get_bulk_preview_items,
 )
-from employee.views.daily_salary.daily_salary_delete_bulk.messages.messages_bulk_preview import (
-    preview_items,
+from employee.views.daily_salary.daily_salary_delete_bulk.messages.delete_bulk_messange import (
+    delete_bulk_message,
 )
 
 
@@ -39,20 +39,20 @@ def daily_salary_delete_bulk(request):
         messages.warning(request, _("Please select at least one record."))
         return redirect(request.META.get("HTTP_REFERER") or fallback_url)
 
-    blocked_qs, deletable_qs = get_deletable_and_blocked_qs(ids, department)
+    blocked_qs, deletable_qs = get_deletable_and_blocked_daily_salaries(ids, department)
 
     deletable_count = deletable_qs.count()
-    deletable_items, deletable_tail = preview_items(deletable_qs, deletable_count)
+    deletable_items, deletable_tail = get_bulk_preview_items(deletable_qs, deletable_count)
 
     blocked_count = blocked_qs.count()
-    blocked_items, blocked_tail = preview_items(blocked_qs, blocked_count)
+    blocked_items, blocked_tail = get_bulk_preview_items(blocked_qs, blocked_count)
 
     deleted_count = 0
     if deletable_qs.exists():
         deleted_count, _deleted_details = deletable_qs.delete()
 
-    delete_bulk_messages(request, deleted_count, deletable_items, deletable_tail)
+    delete_bulk_message(request, deleted_count, deletable_items, deletable_tail)
 
-    block_bulk_messages(request, blocked_count, blocked_items, blocked_tail)
+    blocked_bulk_message(request, blocked_count, blocked_items, blocked_tail)
 
     return redirect(request.META.get("HTTP_REFERER") or fallback_url)
