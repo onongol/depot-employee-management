f6339620ff19e9528a017a19b5c95cec657e1bdf
onongol <rozserbatlin@gmail.com>
2026-01-29 13:17:05 +0800

    Add extract_work_names function to retrieve and sort work names from results

---
+++ employee/views/daily_work/daily_work_create/messages/work_message_helper.py
@@ -0,0 +1,8 @@
+def extract_work_names(results, works_dict):
+    return sorted(
+        {
+            works_dict.get(str(result["work_id"])).work_name
+            for result in results
+            if str(result.get("work_id")) in works_dict
+        }
+    )
