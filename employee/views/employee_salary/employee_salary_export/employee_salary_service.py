0c3b4f8fd99ab779c55ba560437545ae05ae6616
onongol <rozserbatlin@gmail.com>
2026-01-29 14:39:51 +0800

    Refactor get_employee_salaries to improve variable naming and enhance code clarity

--- employee/views/employee_salary/employee_salary_export/employee_salary_service.py
+++ employee/views/employee_salary/employee_salary_export/employee_salary_service.py
@@ -9,25 +9,25 @@ from employee.views.employee_salary.employee_salary_sort import apply_ordering
 
 
 def get_employee_salaries(request):
-    es_context = employee_salaries_prepare(request)
+    context = employee_salaries_prepare(request)
 
-    employees = es_context.employees
+    employees = context.employees
 
     employees = filter_employees_salary(
         employees,
-        context=es_context
+        context=context
     )
 
     employee_salaries = calculate_employee_salaries(
         employees,
-        context=es_context,
-        wagon_number=es_context.wagon_number if es_context.wagon_mode else None,
+        context=context,
+        wagon_number=context.wagon_number if context.wagon_mode else None,
     )
 
     apply_ordering(
         employee_salaries,
-        context=es_context,
+        context=context,
         allowed_fields=["employee_id", "month", "year"],
     )
 
-    return employee_salaries, es_context
+    return employee_salaries, context
