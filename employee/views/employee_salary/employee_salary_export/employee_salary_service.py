5a77497729d9afc0b3c9a0716a06b4d15df70353
onongol <rozserbatlin@gmail.com>
2026-01-29 13:39:13 +0800

    Refactor get_employee_salaries to use calculate_employee_salaries function for improved clarity and maintainability

--- employee/views/employee_salary/employee_salary_export/employee_salary_service.py
+++ employee/views/employee_salary/employee_salary_export/employee_salary_service.py
@@ -1,6 +1,6 @@
 from employee.utils.filters import filter_employees_salary
-from employee.views.employee_salary.employee_salary_calculate.employee_salary_calculate import (
-    employee_salary_calculate,
+from employee.views.employee_salary.calculation.calculate_employee_salaries import (
+    calculate_employee_salaries,
 )
 from employee.views.employee_salary.employee_salary_prepare import (
     employee_salaries_prepare,
@@ -18,7 +18,7 @@ def get_employee_salaries(request):
         context=es_context
     )
 
-    employee_salaries = employee_salary_calculate(
+    employee_salaries = calculate_employee_salaries(
         employees,
         context=es_context,
         wagon_number=es_context.wagon_number if es_context.wagon_mode else None,
