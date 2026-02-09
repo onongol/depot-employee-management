176fc24918b7ce3ba2c7ac660e78b55c75e53e34
onongol <rozserbatlin@gmail.com>
2026-02-09 09:05:39 +0800

    Refactor success messages in WorkCreateView and WorkUpdateView for consistency and clarity

--- employee/views/work/work_views.py
+++ employee/views/work/work_views.py
@@ -8,6 +8,7 @@ from employee.mixins.block_delete_mixins import BlockDeleteMixin
 from employee.mixins.context_mixins import WorkContextMixin
 from employee.mixins.delete_protection_mixins import DeleteProtectionMixin
 from employee.mixins.department_form_mixins import FormDepartmentMixin
+from employee.mixins.department_mixins import InitialDepartmentMixin
 from employee.mixins.permissions_mixins import OnlyAdminMixin
 from employee.mixins.wagon_context_mixins import WagonContextMixin
 from employee.models import Piecework
@@ -20,12 +21,18 @@ class WorkCreateView(
     WagonContextMixin,
     SuccessMessageMixin,
     FormDepartmentMixin,
+    InitialDepartmentMixin,
     CreateView,
 ):
     login_url = "login"
     form_class = WorkForm
     template_name = "work/work_create.html"
-    success_message = _("Work %(work_name)s created successfully.")
+    success_message = _("Created %(object_name)s")
+
+    def get_success_message(self, _cleaned_data):
+        return self.success_message % {
+            "object_name": self.get_object_name(self.object),
+        }
 
 
 class WorkUpdateView(
@@ -35,12 +42,13 @@ class WorkUpdateView(
     WagonContextMixin,
     SuccessMessageMixin,
     FormDepartmentMixin,
+    InitialDepartmentMixin,
     UpdateView,
 ):
     login_url = "login"
     form_class = UpdateWorkForm
     template_name = "work/work_update.html"
-    success_message = _("Work updated successfully.")
+    success_message = _("Updated")
 
 
 class WorkDeleteView(
@@ -62,6 +70,3 @@ class WorkDeleteView(
     # Handle the deletion and send a warning.
     def get_redirect_url(self):
         return self.success_url
-
-    def get_object_name(self):
-        return f"{self.object.work_name}"
