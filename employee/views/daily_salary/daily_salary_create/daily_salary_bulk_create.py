35d3bd08557fac3a93b8c10c2d660fd9ef33aada
onongol <rozserbatlin@gmail.com>
2026-02-09 08:50:16 +0800

    Refactor error logging and messages in bulk_daily_salary_create for consistency

--- employee/views/daily_salary/daily_salary_create/daily_salary_bulk_create.py
+++ employee/views/daily_salary/daily_salary_create/daily_salary_bulk_create.py
@@ -11,7 +11,7 @@ def bulk_daily_salary_create(new_records, errors):
         with transaction.atomic():
             DailySalary.objects.bulk_create(new_records)
     except Exception:
-        logging.exception("Bulk create DailySalary failed")
-        errors.append(_("Error saving daily salary records."))
+        logging.exception("Bulk create failed")
+        errors.append(_("Error saving records."))
         return False
     return True
