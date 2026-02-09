88a8aef628756b4cf9597fc94556e7a25ae9c84e
onongol <rozserbatlin@gmail.com>
2026-02-09 08:45:09 +0800

    Refactor delete protection mixin to use success message for deletion confirmation

--- employee/mixins/context_mixins.py
+++ employee/mixins/context_mixins.py
@@ -10,7 +10,6 @@ class EmployeeContextMixin(GenericContextMixin):
     object_type = _("Employee")
     success_url = reverse_lazy("employee_list")
     cancel_url = reverse_lazy("employee_list")
-    object_name_func = staticmethod(lambda obj: f"{obj.employee_id}/{obj.name}")
 
 
 class WorkContextMixin(GenericContextMixin):
@@ -18,7 +17,6 @@ class WorkContextMixin(GenericContextMixin):
     object_type = _("Work")
     success_url = reverse_lazy("work_list")
     cancel_url = reverse_lazy("work_list")
-    object_name_func = staticmethod(lambda obj: str(obj))
 
 
 class DailySalaryContextMixin(GenericContextMixin):
@@ -26,9 +24,6 @@ class DailySalaryContextMixin(GenericContextMixin):
     object_type = _("Daily Salary")
     success_url = reverse_lazy("daily_salary_list")
     cancel_url = reverse_lazy("daily_salary_list")
-    object_name_func = staticmethod(
-        lambda obj: f"{obj.employee.employee_id}/{obj.employee.name}/{obj.salary_date}"
-    )
 
 
 class DailyWorkContextMixin(GenericContextMixin):
@@ -36,9 +31,6 @@ class DailyWorkContextMixin(GenericContextMixin):
     object_type = _("Daily Work")
     success_url = reverse_lazy("daily_work_list")
     cancel_url = reverse_lazy("daily_work_list")
-    object_name_func = staticmethod(
-        lambda obj: f"{obj.work.work_name}/{obj.type_work}/{obj.work_date}"
-    )
 
 
 class PieceworkContextMixin(GenericContextMixin):
@@ -46,9 +38,3 @@ class PieceworkContextMixin(GenericContextMixin):
     object_type = _("Piecework")
     success_url = reverse_lazy("piecework_list")
     cancel_url = reverse_lazy("piecework_list")
-    object_name_func = staticmethod(
-        lambda obj: (
-            f"{obj.employee.employee_id}/{obj.employee.name} "
-            f"{obj.work.work_name}/{obj.type_work}/{obj.work_date}"
-        )
-    )
