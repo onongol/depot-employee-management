e0b8b7f6c2c762eb86380578efec7b03e6554ad8
onongol <rozserbatlin@gmail.com>
2026-02-09 08:45:31 +0800

    Refactor delete warning mixin to send success message upon deletion

--- employee/mixins/delete_warning_mixins.py
+++ employee/mixins/delete_warning_mixins.py
@@ -1,4 +1,4 @@
-from employee.messages.delete_warning import send_delete_warning
+from employee.messages.delete_success_message import send_delete_success_message
 
 
 class DeleteWarningMixin:
@@ -8,5 +8,5 @@ class DeleteWarningMixin:
         self.object = self.get_object()
         object_name = self.get_object_name()
         response = super().post(request, *args, **kwargs)
-        send_delete_warning(request, object_name)
+        send_delete_success_message(request, object_name)
         return response
