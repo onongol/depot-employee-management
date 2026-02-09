80a58d4c2e288e6b20045df56e3b11755daabd72
onongol <rozserbatlin@gmail.com>
2026-02-09 14:05:46 +0800

    Refactor home view to check user authentication before rendering templates

--- employee/views/home/home_views.py
+++ employee/views/home/home_views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 def home(request):    
     department = request.GET.get("department") or request.session.get("department")
 
-    return render(request, "home/home.html", {"selected_department": department})
+    if request.user.is_authenticated:
+        return render(request, "home/home.html", {"selected_department": department})
+    return render(request, "home/home_signin.html")
