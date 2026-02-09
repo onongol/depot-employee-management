641f543550a948147f40a344a148a8d00eae1ff0
onongol <rozserbatlin@gmail.com>
2026-02-09 08:56:07 +0800

    Refactor work name extraction in extract_work_names for clarity

--- employee/views/daily_work/daily_work_create/messages/work_message_helper.py
+++ employee/views/daily_work/daily_work_create/messages/work_message_helper.py
@@ -1,7 +1,7 @@
 def extract_work_names(results, works_dict):
     return sorted(
         {
-            works_dict.get(str(result["work_id"])).work_name
+            str(works_dict[str(result["work_id"])])
             for result in results
             if str(result.get("work_id")) in works_dict
         }
