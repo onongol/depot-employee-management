d92a16ee62aa0bf61cd7e9bdb32e62d2ee7337ba
onongol <rozserbatlin@gmail.com>
2026-02-09 09:16:35 +0800

    Remove delete block message for clarity and redundancy

--- employee/messages/delete_block.py
+++
@@ -1,7 +0,0 @@
-from django.utils.translation import gettext_lazy as _
-
-
-DELETE_BLOCK_MESSAGE = _(
-    "Cannot delete %(object_name)s because it is associated with %(related)s records. "
-    "Please remove the related %(related)s records first."
-)
