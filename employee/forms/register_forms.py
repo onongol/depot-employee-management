66cac4e2dc6cb3aa0fd93b7d8bc20b8f84e2ede4
onongol <rozserbatlin@gmail.com>
2026-02-24 14:00:59 +0800

    Add username and password fields to custom user registration form

--- employee/forms/register_forms.py
+++ employee/forms/register_forms.py
@@ -21,6 +21,26 @@ class CustomUserCreationForm(UserCreationForm):
         help_text=_("Enter your ID to sync your profile."),
     )
 
+    username = forms.CharField(
+        label=_("Username"),
+        max_length=150,
+        help_text="",
+    )
+
+    password1 = forms.CharField(
+        label=_("Password"),
+        strip=False,
+        widget=forms.PasswordInput,
+        help_text=_("Minimum of 8 characters."),
+    )
+
+    password2 = forms.CharField(
+        label=_("Password confirmation"),
+        widget=forms.PasswordInput,
+        strip=False,
+        help_text="",
+    )
+
     class Meta:
         model = User
         fields = ("employee_id", "username", "password1", "password2")
