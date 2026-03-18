548e55139ac510137ae39c7a2851c93dd71d6dd1
onongol <rozserbatlin@gmail.com>
2026-03-18 14:14:47 +0800

    Refactor auto_hide_alert.ts to rename constants for clarity and consistency

--- assets/js/alert/auto_hide_alert.ts
+++ assets/js/alert/auto_hide_alert.ts
@@ -1,19 +1,19 @@
-const ALERT_SELECTORS = {
+const ALERT_HIDE_SELECTORS = {
 	alert: "[data-auto-hide-alert]",
 } as const;
 
-const ALERT_CLASSES = {
+const ALERT_HIDE_CLASSES = {
 	hidden: "hidden",
 } as const;
 
-const ALERT_TIMEOUT = 5000 as const;
+const ALERT_HIDE_TIMEOUT = 5000 as const;
 
 document.addEventListener("DOMContentLoaded", () => {
 	for (const alert of document.querySelectorAll<HTMLElement>(
-		ALERT_SELECTORS.alert,
+		ALERT_HIDE_SELECTORS.alert,
 	)) {
 		setTimeout(() => {
-			alert.classList.add(ALERT_CLASSES.hidden);
-		}, ALERT_TIMEOUT);
+			alert.classList.add(ALERT_HIDE_CLASSES.hidden);
+		}, ALERT_HIDE_TIMEOUT);
 	}
 });
