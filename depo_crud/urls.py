2f4dbfbd8b4c518d3d746f8d97386f429d131564
onongol <rozserbatlin@gmail.com>
2026-03-19 10:41:52 +0800

    Add health check endpoint for application status

--- depo_crud/urls.py
+++ depo_crud/urls.py
@@ -15,12 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.views.i18n import JavaScriptCatalog
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("ok")
 
 urlpatterns = [
+    path("health/", health_check),
     path("i18n/", include("django.conf.urls.i18n")),
     path("jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
     path("admin/", admin.site.urls),
