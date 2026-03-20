ce4d6b002513b09738e01a0808bc48beb0747450
onongol <rozserbatlin@gmail.com>
2026-03-20 09:01:12 +0800

    Add navbar_page_types to context processors for improved layout handling

--- employee/context_processors/__init__.py
+++ employee/context_processors/__init__.py
@@ -1,9 +1,11 @@
 from .department_warning import needs_department_warning
 from .departments import global_departments
+from .navbar_page_types import navbar_page_types
 from .permissions import is_employee, is_master, is_payroll
 
 __all__ = [
     "global_departments",
+    "navbar_page_types",
     "needs_department_warning",
     "is_employee",
     "is_master",
