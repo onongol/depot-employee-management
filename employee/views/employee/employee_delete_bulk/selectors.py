09a2b3803981a77f1b27ce17925981132630c5b6
onongol <rozserbatlin@gmail.com>
2026-03-24 13:28:29 +0800

    Refactor employee deletion logic to remove DailyWork checks and simplify queryset filtering

--- employee/views/employee/employee_delete_bulk/selectors.py
+++ employee/views/employee/employee_delete_bulk/selectors.py
@@ -7,16 +7,14 @@ from employee.models.piecework_models import Piecework
 
 def get_deletable_and_blocked_employees(ids, department):
     """Splits Employee queryset into blocked and deletable sets based on related DailyWork or Piecework existence for bulk operations."""
-    dailywork_exists = DailyWork.objects.filter(work=OuterRef("pk"))
-    piecework_exists = Piecework.objects.filter(work=OuterRef("pk"))
+    piecework_exists = Piecework.objects.filter(employee=OuterRef("pk"))
 
     base_qs = Employee.objects.filter(pk__in=ids, department=department).annotate(
-        has_dailywork=Exists(dailywork_exists),
         has_piecework=Exists(piecework_exists),
     )
 
-    blocked_qs = base_qs.filter(has_dailywork=True, has_piecework=True)
+    blocked_qs = base_qs.filter(has_piecework=True)
     blocked_qs = blocked_qs.distinct()
-    deletable_qs = base_qs.filter(has_dailywork=False, has_piecework=False)
+    deletable_qs = base_qs.filter(has_piecework=False)
 
     return blocked_qs, deletable_qs
