ee57141bdaaf0ac595d9255c05438679af644b6c
onongol <rozserbatlin@gmail.com>
2026-04-06 14:54:17 +0800

    Enhance Work views by integrating logging mixins for create, update, and delete operations

--- employee/views/work/work_views.py
+++ employee/views/work/work_views.py
@@ -6,11 +6,14 @@ from django.views.generic import CreateView, DeleteView, UpdateView
 from employee.forms import UpdateWorkForm, WorkForm
 from employee.mixins.block_delete_mixins import BlockDeleteMixin
 from employee.mixins.context_mixins import WorkContextMixin
+from employee.mixins.create_mixin import AdminLoggedCreateMixin
+from employee.mixins.delete_mixin import AdminLoggedDeleteMixin
 from employee.mixins.delete_protection_mixins import DeleteProtectionMixin
 from employee.mixins.department_form_mixins import FormDepartmentMixin
 from employee.mixins.department_mixins import InitialDepartmentMixin
 from employee.mixins.mechanic_context_mixins import MechanicContextMixin
 from employee.mixins.permissions_mixins import OnlyAdminMixin
+from employee.mixins.update_mixin import AdminLoggedUpdateMixin
 from employee.mixins.wagon_context_mixins import WagonContextMixin
 from employee.models import Piecework
 
@@ -24,9 +27,9 @@ class WorkCreateView(
     SuccessMessageMixin,
     FormDepartmentMixin,
     InitialDepartmentMixin,
+    AdminLoggedCreateMixin,
     CreateView,
 ):
-    login_url = "login"
     form_class = WorkForm
     template_name = "work/work_create.html"
     success_message = _("Created %(object_name)s")
@@ -46,9 +49,9 @@ class WorkUpdateView(
     SuccessMessageMixin,
     FormDepartmentMixin,
     InitialDepartmentMixin,
+    AdminLoggedUpdateMixin,
     UpdateView,
 ):
-    login_url = "login"
     form_class = UpdateWorkForm
     template_name = "work/work_update.html"
     success_message = _("Updated")
@@ -60,16 +63,12 @@ class WorkDeleteView(
     WorkContextMixin,
     BlockDeleteMixin,
     DeleteProtectionMixin,
+    AdminLoggedDeleteMixin,
     DeleteView,
 ):
-    login_url = "login"
     template_name = "work/work_confirm_delete.html"
     block_related_models = [_("Daily Work"), _("Piecework")]
 
     # Get related piecework records to check if deletion is allowed.
     def get_related_objects(self):
         return Piecework.objects.filter(work=self.object)
-
-    # Handle the deletion and send a warning.
-    def get_redirect_url(self):
-        return self.success_url
