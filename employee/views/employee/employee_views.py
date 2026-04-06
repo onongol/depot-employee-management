52e28d80b31b2a5481c1d69415fef7c43804aaa3
onongol <rozserbatlin@gmail.com>
2026-04-06 15:14:39 +0800

    Enhance employee views with admin logging mixins for create, update, and delete operations

--- employee/views/employee/employee_views.py
+++ employee/views/employee/employee_views.py
@@ -6,9 +6,12 @@ from django.views.generic import CreateView, DeleteView, UpdateView
 from employee.forms import EmployeeForm, UpdateEmployeeForm
 from employee.mixins.block_delete_mixins import BlockDeleteMixin
 from employee.mixins.context_mixins import EmployeeContextMixin
+from employee.mixins.create_mixin import AdminLoggedCreateMixin
+from employee.mixins.delete_mixin import AdminLoggedDeleteMixin
 from employee.mixins.delete_protection_mixins import DeleteProtectionMixin
 from employee.mixins.department_mixins import InitialDepartmentMixin
 from employee.mixins.permissions_mixins import OnlyAdminMixin
+from employee.mixins.update_mixin import AdminLoggedUpdateMixin
 from employee.models import DailySalary
 
 
@@ -18,9 +21,9 @@ class EmployeeCreateView(
     EmployeeContextMixin,
     InitialDepartmentMixin,
     SuccessMessageMixin,
+    AdminLoggedCreateMixin,
     CreateView,
 ):
-    login_url = "login"
     form_class = EmployeeForm
     template_name = "employee/employee_create.html"
     success_message = _("Created %(object_name)s")
@@ -37,9 +40,9 @@ class EmployeeUpdateView(
     EmployeeContextMixin,
     InitialDepartmentMixin,
     SuccessMessageMixin,
+    AdminLoggedUpdateMixin,
     UpdateView,
 ):
-    login_url = "login"
     form_class = UpdateEmployeeForm
     template_name = "employee/employee_update.html"
     success_message = _("Updated")
@@ -51,15 +54,12 @@ class EmployeeDeleteView(
     EmployeeContextMixin,
     BlockDeleteMixin,
     DeleteProtectionMixin,
+    AdminLoggedDeleteMixin,
     DeleteView,
 ):
-    login_url = "login"
     template_name = "employee/employee_confirm_delete.html"
     block_related_models = [_("Daily Salary"), _("Daily Work"), _("Piecework")]
 
     # Get related daily salary records to check if deletion is allowed.
     def get_related_objects(self):
         return DailySalary.objects.filter(employee=self.object)
-
-    def get_redirect_url(self):
-        return self.success_url
