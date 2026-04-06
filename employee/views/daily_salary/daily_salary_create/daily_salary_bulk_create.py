0888ce0d9572e4f5605c93298fa67dcec70d018d
onongol <rozserbatlin@gmail.com>
2026-04-06 14:36:59 +0800

    Enhance bulk_daily_salary_create to support user tracking with history logging

--- employee/views/daily_salary/daily_salary_create/daily_salary_bulk_create.py
+++ employee/views/daily_salary/daily_salary_create/daily_salary_bulk_create.py
@@ -2,14 +2,15 @@ import logging
 
 from django.db import transaction
 from django.utils.translation import gettext_lazy as _
+from simple_history.utils import bulk_create_with_history
 
 from employee.models.daily_salary_models import DailySalary
 
 
-def bulk_daily_salary_create(new_records, errors):
+def bulk_daily_salary_create(new_records, errors, user=None):
     try:
         with transaction.atomic():
-            DailySalary.objects.bulk_create(new_records)
+            bulk_create_with_history(new_records, DailySalary, default_user=user)
     except Exception:
         logging.exception("Bulk create failed")
         errors.append(_("Error saving records."))
