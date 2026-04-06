6836afcda97440dd595bfd1a33ac93dba3c1a93c
onongol <rozserbatlin@gmail.com>
2026-04-06 11:27:13 +0800

    Add logging for DailyWork updates and deletions in admin views

--- employee/views/daily_work/daily_work_views.py
+++ employee/views/daily_work/daily_work_views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.db import transaction
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import DeleteView, UpdateView
@@ -11,6 +13,10 @@ from employee.mixins.delete_warning_mixins import DeleteWarningMixin
 from employee.mixins.department_form_mixins import FormDepartmentMixin
 from employee.mixins.permissions_mixins import OnlyAdminMixin
 from employee.models import DailyWork
+from employee.services.admin_log_entries import (
+    log_object_change,
+    log_object_deletion,
+)
 from employee.utils.access import is_creater
 from employee.views.daily_work.daily_work_create.daily_work_piecework_create import (
     daily_work_piecework_create,
@@ -31,6 +37,13 @@ class DailyWorkUpdateView(
     template_name = "daily_work/daily_work_piecework_update.html"
     success_message = _("Updated")
 
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        log_object_change(
+            self.request.user, self.object, changed_fields=form.changed_data
+        )
+        return response
+
     def get_success_url(self):
         return reverse("daily_work_list")
 
@@ -48,7 +61,14 @@ class DailyWorkDeleteView(
 
     def delete(self, request, *args, **kwargs):
         """Handle the deletion of a DailyWork entry."""
-        return super().delete(request, *args, **kwargs)
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+
+        with transaction.atomic():
+            log_object_deletion(request.user, self.object)
+            self.object.delete()
+
+        return HttpResponseRedirect(success_url)
 
     def get_success_url(self):
         return reverse("daily_work_list")
