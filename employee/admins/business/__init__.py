60ba86016366ae07ab50c94a221c99c176d9c350
onongol <rozserbatlin@gmail.com>
2026-04-07 14:14:49 +0800

    Add __init__.py to employee.admins.business for module initialization

---
+++ employee/admins/business/__init__.py
@@ -0,0 +1,19 @@
+from employee.admins.business import (
+    daily_salary_admin,
+    daily_work_admin,
+    employee_admin,
+    masret_admin,
+    payroll_admin,
+    piecework_admin,
+    work_admin,
+)
+
+__all__ = [
+    "daily_salary_admin",
+    "daily_work_admin",
+    "employee_admin",
+    "masret_admin",
+    "payroll_admin",
+    "piecework_admin",
+    "work_admin",
+]
