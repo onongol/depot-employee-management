fe3781cac7e94376a1ea5ac83b205d1fd3722422
onongol <rozserbatlin@gmail.com>
2026-04-07 14:15:17 +0800

    Add PayrollAdmin class for managing payroll records in the admin interface

---
+++ employee/admins/business/payroll_admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from simple_history.admin import SimpleHistoryAdmin
+from unfold.admin import ModelAdmin
+
+from employee.admins.common import ImportExportMixin
+from employee.models import Payroll
+
+
+@admin.register(Payroll)
+class PayrollAdmin(
+    ModelAdmin, SimpleHistoryAdmin, ImportExportModelAdmin, ImportExportMixin
+):
+    list_display = (
+        "payroll_id",
+        "name",
+        "is_active",
+    )
+    search_fields = (
+        "payroll_id",
+        "name",
+    )
+    search_help_text = "Search by payroll ID or name"
+    list_filter = ["is_active"]
+    list_editable = ("is_active",)
+    ordering = ("-payroll_id",)
+    list_display_links = (
+        "payroll_id",
+        "name",
+    )
