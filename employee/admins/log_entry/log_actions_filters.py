97ac472e4103e8a522dab4f9e121827040941b7d
onongol <rozserbatlin@gmail.com>
2026-04-07 14:15:56 +0800

    Add LogEntryActionFilter for filtering log entries by action type in admin interface

---
+++ employee/admins/log_entry/log_actions_filters.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.contrib.admin.models import ADDITION, CHANGE, DELETION
+from django.utils.translation import gettext_lazy as _
+
+
+class LogEntryActionFilter(admin.SimpleListFilter):
+    title = _("Action")
+    parameter_name = "action_flag"
+
+    def lookups(self, request, model_admin):
+        return (
+            (str(ADDITION), _("Added")),
+            (str(CHANGE), _("Changed")),
+            (str(DELETION), _("Deleted")),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(action_flag=self.value())
+        return queryset
