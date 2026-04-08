23c61d3dd10298d69250ca649172e2d02aa95899
onongol <rozserbatlin@gmail.com>
2026-04-08 08:30:45 +0800

    Add docstring to message_text function for clarity

--- employee/admins/log_entry/message_text.py
+++ employee/admins/log_entry/message_text.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 @admin.display(description="Message")
 def message_text(self, obj):
+    """Admin display callback to show the change message or a default label for changes."""
     message = obj.get_change_message()
     if message:
         return message
