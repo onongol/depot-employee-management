490267996a0c177e3e6267558debfbb49a1c6e1f
onongol <rozserbatlin@gmail.com>
2026-04-10 15:28:11 +0800

    Remove WorkDeleteView and related imports as part of soft delete implementation

--- employee/views/work/work_views.py
+++ employee/views/work/work_views.py
@@ -1,21 +1,17 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import CreateView, DeleteView, UpdateView
+from django.views.generic import CreateView, UpdateView
 
 from employee.forms import UpdateWorkForm, WorkForm
-from employee.mixins.block_delete_mixins import BlockDeleteMixin
 from employee.mixins.context_mixins import WorkContextMixin
 from employee.mixins.create_mixin import AdminLoggedCreateMixin
-from employee.mixins.delete_mixin import AdminLoggedDeleteMixin
-from employee.mixins.delete_protection_mixins import DeleteProtectionMixin
 from employee.mixins.department_form_mixins import FormDepartmentMixin
 from employee.mixins.department_mixins import InitialDepartmentMixin
 from employee.mixins.mechanic_context_mixins import MechanicContextMixin
 from employee.mixins.permissions_mixins import OnlyAdminMixin
 from employee.mixins.update_mixin import AdminLoggedUpdateMixin
 from employee.mixins.wagon_context_mixins import WagonContextMixin
-from employee.models import Piecework
 
 
 class WorkCreateView(
@@ -55,20 +51,3 @@ class WorkUpdateView(
     form_class = UpdateWorkForm
     template_name = "work/work_update.html"
     success_message = _("Updated")
-
-
-class WorkDeleteView(
-    LoginRequiredMixin,
-    OnlyAdminMixin,
-    WorkContextMixin,
-    BlockDeleteMixin,
-    DeleteProtectionMixin,
-    AdminLoggedDeleteMixin,
-    DeleteView,
-):
-    template_name = "work/work_confirm_delete.html"
-    block_related_models = [_("Daily Work"), _("Piecework")]
-
-    # Get related piecework records to check if deletion is allowed.
-    def get_related_objects(self):
-        return Piecework.objects.filter(work=self.object)
