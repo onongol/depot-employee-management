ea5c997c052d927f36d0954cece59f95d756f507
onongol <rozserbatlin@gmail.com>
2026-04-10 15:34:32 +0800

    Remove PieceworkDeleteView and related imports as part of soft delete implementation

--- employee/views/piecework/piecework_views.py
+++ employee/views/piecework/piecework_views.py
@@ -1,21 +1,6 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DeleteView
 
-from employee.mixins.context_mixins import PieceworkContextMixin
-from employee.mixins.delete_warning_mixins import DeleteWarningMixin
-from employee.mixins.permissions_mixins import OnlyAdminMixin
 from employee.utils.access import is_creater
-
-
-class PieceworkDeleteView(
-    LoginRequiredMixin,
-    OnlyAdminMixin,
-    PieceworkContextMixin,
-    DeleteWarningMixin,
-    DeleteView,
-):
-    template_name = "piecework/piecework_delete.html"
 
 
 @login_required
