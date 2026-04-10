c05633b4535fb0015b74fc3cba45ead72ef82eb0
onongol <rozserbatlin@gmail.com>
2026-04-10 15:11:17 +0800

    Refactor work_delete_bulk to remove blocked works handling and simplify deletion logic

--- employee/views/work/work_delete_bulk/work_delete_bulk.py
+++ employee/views/work/work_delete_bulk/work_delete_bulk.py
@@ -5,22 +5,17 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.decorators.http import require_POST
 
-from employee.messages.bulk_delete_messages.blocked_bulk_message import (
-    blocked_bulk_message,
-)
 from employee.messages.bulk_delete_messages.bulk_preview_helpers import (
     get_bulk_preview_items,
 )
 from employee.messages.bulk_delete_messages.delete_bulk_message import (
     delete_bulk_message,
 )
+from employee.models.work_models import Work
 from employee.services.admin_log_delete import delete_queryset_with_admin_log
 from employee.utils.access import is_admin
 from employee.utils.parse_ids import parse_ids
 from employee.utils.select_department import get_selected_department
-from employee.views.work.work_delete_bulk.selectors import (
-    get_deletable_and_blocked_works,
-)
 
 
 @require_POST
@@ -35,15 +30,11 @@ def work_delete_bulk(request):
         messages.warning(request, _("Select at least one record."))
         return redirect(request.META.get("HTTP_REFERER") or fallback_url)
 
-    blocked_qs, deletable_qs = get_deletable_and_blocked_works(ids, department)
-
+    deletable_qs = Work.all_objects.filter(pk__in=ids, department=department)
     deletable_count = deletable_qs.count()
     deletable_items, deletable_tail = get_bulk_preview_items(
         deletable_qs, deletable_count
     )
-
-    blocked_count = blocked_qs.count()
-    blocked_items, blocked_tail = get_bulk_preview_items(blocked_qs, blocked_count)
 
     deleted_count = 0
     if deletable_qs.exists():
@@ -52,6 +43,5 @@ def work_delete_bulk(request):
         )
 
     delete_bulk_message(request, deleted_count, deletable_items, deletable_tail)
-    blocked_bulk_message(request, blocked_count, blocked_items, blocked_tail)
 
     return redirect(request.META.get("HTTP_REFERER") or fallback_url)
