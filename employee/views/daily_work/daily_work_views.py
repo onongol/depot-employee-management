fcb8a3160252b66aa361e2cf2139b279de2a0eda
onongol <rozserbatlin@gmail.com>
2026-04-10 15:41:08 +0800

    Remove DailyWorkDeleteView and related imports as part of soft delete implementation

--- employee/views/daily_work/daily_work_views.py
+++ employee/views/daily_work/daily_work_views.py
@@ -2,12 +2,10 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import DeleteView, UpdateView
+from django.views.generic import UpdateView
 
 from employee.forms.daily_work_forms import UpdateDailyWorkForm
 from employee.mixins.context_mixins import DailyWorkContextMixin
-from employee.mixins.delete_mixin import AdminLoggedDeleteMixin
-from employee.mixins.delete_warning_mixins import DeleteWarningMixin
 from employee.mixins.department_form_mixins import FormDepartmentMixin
 from employee.mixins.permissions_mixins import OnlyAdminMixin
 from employee.mixins.update_mixin import AdminLoggedUpdateMixin
@@ -29,17 +27,6 @@ class DailyWorkUpdateView(
     form_class = UpdateDailyWorkForm
     template_name = "daily_work/daily_work_piecework_update.html"
     success_message = _("Updated")
-
-
-class DailyWorkDeleteView(
-    LoginRequiredMixin,
-    OnlyAdminMixin,
-    DailyWorkContextMixin,
-    DeleteWarningMixin,
-    AdminLoggedDeleteMixin,
-    DeleteView,
-):
-    template_name = "daily_work/daily_work_confirm_delete.html"
 
 
 @login_required
