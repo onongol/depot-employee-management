0c28832fe9476f813e70948acc3fe14912fdf05f
onongol <rozserbatlin@gmail.com>
2026-04-10 13:30:58 +0800

    Add SoftDeleteQuerySet to implement soft delete functionality for model instances

---
+++ employee/services/soft_delete_queryset.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class SoftDeleteQuerySet(models.QuerySet):
+    def delete(self):
+        return super().update(is_deleted=True)
+
+    def hard_delete(self):
+        return super().delete()
+
+    def alive(self):
+        return self.filter(is_deleted=False)
+
+    def dead(self):
+        return self.filter(is_deleted=True)
