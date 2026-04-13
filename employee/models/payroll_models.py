0f83832e5d1dc9ed94cf905b4059bbb45d4472dd
onongol <rozserbatlin@gmail.com>
2026-04-13 09:47:03 +0800

    Implement soft delete support in Payroll model by adding is_deleted field and integrating SoftDeleteMixin and SoftDeleteManager

--- employee/models/payroll_models.py
+++ employee/models/payroll_models.py
@@ -3,8 +3,11 @@ from django.core.validators import MinValueValidator
 from django.db import models
 from simple_history.models import HistoricalRecords
 
+from employee.models.models_mixins.soft_delete_mixin import SoftDeleteMixin
+from employee.services.soft_delete_manager import SoftDeleteManager
 
-class Payroll(models.Model):
+
+class Payroll(SoftDeleteMixin, models.Model):
     """This model represents a payroll specialist in the system."""
 
     payroll_id = models.IntegerField(
@@ -15,6 +18,9 @@ class Payroll(models.Model):
     # Active status of the employee
     is_active = models.BooleanField(default=True)
 
+    # Soft delete flag: True if the record is considered deleted, False otherwise.
+    is_deleted = models.BooleanField(default=False, db_index=True)
+
     # Connection to the User model
     user = models.OneToOneField(
         User,
@@ -23,7 +29,12 @@ class Payroll(models.Model):
         blank=True,
         related_name="payroll_profile",
     )
+
     history = HistoricalRecords()
+
+    # Custom managers to handle soft deletion logic.
+    objects = SoftDeleteManager()
+    all_objects = SoftDeleteManager(only_alive=False)
 
     def __str__(self):
         return f"(ID: {self.payroll_id}) {self.name}"
