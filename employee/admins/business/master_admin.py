33b3314003e12d702f07dd2055bc991f8dc6f820
onongol <rozserbatlin@gmail.com>
2026-04-13 09:47:22 +0800

    Add MasterAdmin with soft delete support and necessary configurations

---
+++ employee/admins/business/master_admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from simple_history.admin import SimpleHistoryAdmin
+from unfold.admin import ModelAdmin
+
+from employee.admins.common import ImportExportMixin, SoftDeleteAdminMixin
+from employee.models import Master
+
+
+@admin.register(Master)
+class MasterAdmin(
+    SoftDeleteAdminMixin,
+    ModelAdmin,
+    SimpleHistoryAdmin,
+    ImportExportModelAdmin,
+    ImportExportMixin,
+):
+    list_display = (
+        "master_id",
+        "name",
+        "department",
+        "is_active",
+        "is_deleted",
+    )
+    search_fields = (
+        "master_id",
+        "name",
+    )
+    search_help_text = "Search by master ID or name"
+    list_filter = ["department", "is_active", "is_deleted"]
+    list_editable = ("is_active",)
+    ordering = ("-master_id",)
+    list_display_links = (
+        "master_id",
+        "name",
+    )
