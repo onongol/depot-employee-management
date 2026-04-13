0b3423c01bedfdb94be2cfe830120b5e7a6eafc3
onongol <rozserbatlin@gmail.com>
2026-04-13 09:47:08 +0800

    Add soft delete support to PayrollAdmin by integrating SoftDeleteAdminMixin and updating list display and filters

--- employee/admins/business/payroll_admin.py
+++ employee/admins/business/payroll_admin.py
@@ -3,25 +3,30 @@ from import_export.admin import ImportExportModelAdmin
 from simple_history.admin import SimpleHistoryAdmin
 from unfold.admin import ModelAdmin
 
-from employee.admins.common import ImportExportMixin
+from employee.admins.common import ImportExportMixin, SoftDeleteAdminMixin
 from employee.models import Payroll
 
 
 @admin.register(Payroll)
 class PayrollAdmin(
-    ModelAdmin, SimpleHistoryAdmin, ImportExportModelAdmin, ImportExportMixin
+    SoftDeleteAdminMixin,
+    ModelAdmin,
+    SimpleHistoryAdmin,
+    ImportExportModelAdmin,
+    ImportExportMixin,
 ):
     list_display = (
         "payroll_id",
         "name",
         "is_active",
+        "is_deleted",
     )
     search_fields = (
         "payroll_id",
         "name",
     )
     search_help_text = "Search by payroll ID or name"
-    list_filter = ["is_active"]
+    list_filter = ["is_active", "is_deleted"]
     list_editable = ("is_active",)
     ordering = ("-payroll_id",)
     list_display_links = (
