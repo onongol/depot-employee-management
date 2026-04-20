b1da5c052c463dca96c67a9bf1299878e3a53332
onongol <rozserbatlin@gmail.com>
2026-04-20 14:14:56 +0800

    Refactor piecework_create_bulk: Streamline employee and work data assignment

--- employee/views/daily_work/daily_work_create/piecework_create_bulk.py
+++ employee/views/daily_work/daily_work_create/piecework_create_bulk.py
@@ -5,20 +5,17 @@ from employee.models import Piecework
 
 def piecework_create_bulk(results, daily_works, works_dict, employees_map):
     """Create Piecework records for a successful DailyWork batch."""
+
     group_id = str(uuid4())
+
     for data in results:
         work_id = data["work_id"]
         emp_id = data["employee_id"]
 
         data["daily_work"] = daily_works.get(work_id)
+        data["employee"] = employees_map.get(str(emp_id))
+        data["work"] = works_dict.get(str(work_id))
+
         data["group_id"] = group_id
-
-        emp_obj = employees_map.get(str(emp_id))
-        work_obj = works_dict.get(str(work_id))
-
-        # Snapshot fields
-        data["employee_name"] = getattr(emp_obj, "name", None)
-        data["work_name"] = getattr(work_obj, "work_name", None)
-        data["department"] = getattr(emp_obj, "department", None)
 
         Piecework.objects.create(**data)
