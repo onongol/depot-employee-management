adfe4100ec91ddf30771c96abcbd2f7a05bb745b
onongol <rozserbatlin@gmail.com>
2026-04-20 14:13:54 +0800

    Refactor create_daily_salary_instance: Remove emp_id parameter and streamline DailySalary instantiation

--- employee/views/daily_salary/daily_salary_create/daily_salary_create_instance.py
+++ employee/views/daily_salary/daily_salary_create/daily_salary_create_instance.py
@@ -1,13 +1,15 @@
 from employee.models.daily_salary_models import DailySalary
 
 
-def create_daily_salary_instance(emp, emp_id, salary_date, hours_per_day):
+def create_daily_salary_instance(emp, salary_date, hours_per_day):
     salary_day = float(hours_per_day) * float(emp.money_per_hour)
 
     return DailySalary(
-        employee_id=emp_id,
-        employee_name=emp.name,
+        employee=emp,
+        employee_code=emp.employee_id,
+        employee_name=emp.employee_name,
         department=emp.department,
+        job_title=emp.job_title,
         salary_date=salary_date,
         hours_per_day=hours_per_day,
         salary_day=salary_day,
