c07069845263fdabc79b257d5be0f33fbafcd6ea
onongol <rozserbatlin@gmail.com>
2026-04-28 13:56:40 +0800

    Refactor permissions import to consolidate user role checks

--- employee/context_processors/__init__.py
+++ employee/context_processors/__init__.py
@@ -1,13 +1,11 @@
 from .department_warning import needs_department_warning
 from .departments import global_departments
 from .navbar_page_types import navbar_page_types
-from .permissions import is_employee, is_master, is_payroll
+from .permissions import user_roles
 
 __all__ = [
     "global_departments",
     "navbar_page_types",
     "needs_department_warning",
-    "is_employee",
-    "is_master",
-    "is_payroll",
+    "user_roles",
 ]
