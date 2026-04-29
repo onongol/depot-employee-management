6c9e838ec42ce00bade20663df97fd770362f16d
onongol <rozserbatlin@gmail.com>
2026-04-29 10:26:03 +0800

    Refactor piecework_prepare to use get_user_groups for role-based access control

--- employee/views/piecework/piecework_prepare.py
+++ employee/views/piecework/piecework_prepare.py
@@ -1,9 +1,11 @@
+from employee.constants.constants import EMPLOYEE_GROUP
 from employee.models import Piecework
 from employee.utils.group_modes import is_detail_group, is_month_group, is_year_group
 from employee.utils.month_period import parse_month_period
 from employee.utils.select_department import get_selected_department
 from employee.utils.select_type_wagon import get_type_wagon_filter_values
 from employee.utils.selects import get_distinct_values
+from employee.utils.user_roles import get_user_groups
 from employee.utils.wagon_department import is_wagon_department
 from employee.views.piecework.piecework_context import PieceworkContext
 
@@ -12,8 +14,10 @@ def piecework_prepare(request) -> PieceworkContext:
     """Prepare base queryset and filter params for Piecework."""
     department = get_selected_department(request)
 
+    groups = get_user_groups(request)
+
     # Base queryset (same visibility rules as list view)
-    if request.user.groups.filter(name="Employees").exists():
+    if EMPLOYEE_GROUP in groups:
         pieceworks = Piecework.objects.select_related("employee", "work").filter(
             employee__user=request.user,
             employee__department=department,
