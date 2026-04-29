0c62a215dcc33a66d07737bbaff7147bfacb1416
onongol <rozserbatlin@gmail.com>
2026-04-29 13:53:11 +0800

    Refactor user role functions to improve structure and readability

--- employee/utils/user_roles.py
+++ employee/utils/user_roles.py
@@ -1,3 +1,10 @@
+from employee.constants.constants import (
+    IS_EMPLOYEE_KEY,
+    IS_MASTER_KEY,
+    IS_PAYROLL_KEY,
+)
+
+
 def get_user_groups(request) -> set[str]:
     """Returns cached user groups for the current request."""
     if not hasattr(request, "_cached_user_groups"):
@@ -5,3 +12,15 @@ def get_user_groups(request) -> set[str]:
             request.user.groups.values_list("name", flat=True)
         )
     return request._cached_user_groups
+
+
+def is_employee(request) -> bool:
+    return getattr(request, "user_roles", {}).get(IS_EMPLOYEE_KEY, False)
+
+
+def is_master(request) -> bool:
+    return getattr(request, "user_roles", {}).get(IS_MASTER_KEY, False)
+
+
+def is_payroll(request) -> bool:
+    return getattr(request, "user_roles", {}).get(IS_PAYROLL_KEY, False)
