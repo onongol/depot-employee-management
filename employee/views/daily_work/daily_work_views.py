56f100f4f600bd16d79c76c4b2996ef574b79acf
onongol <rozserbatlin@gmail.com>
2026-05-04 13:50:25 +0800

    Refactor DailyWorkUpdateView: update queryset to use select_related for improved data retrieval

--- employee/views/daily_work/daily_work_views.py
+++ employee/views/daily_work/daily_work_views.py
@@ -9,6 +9,7 @@ from employee.mixins.context_mixins import DailyWorkContextMixin
 from employee.mixins.department_form_mixins import FormDepartmentMixin
 from employee.mixins.permissions_mixins import OnlyAdminMixin
 from employee.mixins.update_mixin import AdminLoggedUpdateMixin
+from employee.models.daily_work_models import DailyWork
 from employee.utils.access import is_creater
 from employee.views.daily_work.daily_work_create.daily_work_piecework_create import (
     daily_work_piecework_create,
@@ -24,6 +25,7 @@ class DailyWorkUpdateView(
     AdminLoggedUpdateMixin,
     UpdateView,
 ):
+    queryset = DailyWork.objects.select_related("work")
     form_class = UpdateDailyWorkForm
     template_name = "daily_work/daily_work_piecework_update.html"
     success_message = _("Updated")
