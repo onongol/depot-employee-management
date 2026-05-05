9b0295fd3bb3b0f49cdf5e7512ea26f1c3945934
onongol <rozserbatlin@gmail.com>
2026-05-05 13:48:36 +0800

    Fix employee_entries function to use employee_code instead of employee_id

--- employee/views/daily_work/daily_work_create/messages/employee_message_helper.py
+++ employee/views/daily_work/daily_work_create/messages/employee_message_helper.py
@@ -4,25 +4,25 @@ from employee.models import Employee
 def get_employee_entries(results):
     """
     Returns a sorted list of unique employee display names (object_name),
-    e.g. "(ID: 1) S.Name", for all unique employee_id values
+    e.g. "(ID: 1) S.Name", for all unique employee_code(employee_id) values
     found in results.
     """
-    employee_ids = {
-        result["employee_id"] for result in results if result.get("employee_id")
+    employee_codes = {
+        result["employee_code"] for result in results if result.get("employee_code")
     }
 
     employees_map = {
         employee.employee_id: str(employee)
-        for employee in Employee.objects.filter(employee_id__in=employee_ids)
+        for employee in Employee.objects.filter(employee_id__in=employee_codes)
     }
 
     entries = {
         employees_map.get(
-            result["employee_id"],
-            f"(ID: {result['employee_id']}) {result.get('employee_name', '')}",
+            result["employee_code"],
+            f"(ID: {result.get('employee_code')}) {result.get('employee_name', '')}",
         )
         for result in results
-        if result.get("employee_id")
+        if result.get("employee_code")
     }
 
     return sorted(entries)
