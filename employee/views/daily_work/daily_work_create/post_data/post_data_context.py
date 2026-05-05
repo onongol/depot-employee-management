693b1d1a39faa6611e1850557b436e8ad833ffc0
onongol <rozserbatlin@gmail.com>
2026-05-05 13:21:53 +0800

    Change work_date type in PostData from Optional[str] to Optional[date]

--- employee/views/daily_work/daily_work_create/post_data/post_data_context.py
+++ employee/views/daily_work/daily_work_create/post_data/post_data_context.py
@@ -1,14 +1,15 @@
 from dataclasses import dataclass
+from datetime import date
 from typing import Dict, List, Optional
 
 
 @dataclass
 class PostData:
-    work_date: Optional[str]
+    work_date: Optional[date]
     type_work: Optional[str]
     job_title: Optional[str]
     wagon_number: Optional[str]
     selected_employee_ids: List[str]
     selected_work_ids: List[str]
     amounts: Dict[str, str]
-    
+
