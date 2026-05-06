685bd3cc94c2d07114fb01cae7ed6487a1a0b44a
onongol <rozserbatlin@gmail.com>
2026-05-06 11:30:16 +0800

    Update model_name display description to use translation for better localization

--- employee/admins/log_entry/model_name.py
+++ employee/admins/log_entry/model_name.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from employee.admins.log_entry.type_label import get_content_type_label
 
 
-@admin.display(description="Model")
-def model_name(self, obj):
+@admin.display(description=_("Model"))
+def model_name(_self, obj):
     """Admin display callback to show the content type (model) label for a LogEntry."""
     return get_content_type_label(obj.content_type)
