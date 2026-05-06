1c82261b470f42b455b9bdee5f4fef696831363b
onongol <rozserbatlin@gmail.com>
2026-05-06 16:48:18 +0800

    Refactor DailySalaryForm and UpdateDailySalaryForm to use COMMON_DAILY_SALARY_WIDGETS for widget definitions

--- employee/forms/daily_salary_forms.py
+++ employee/forms/daily_salary_forms.py
@@ -2,24 +2,26 @@ from django import forms
 
 from employee.models import DailySalary
 
+COMMON_DAILY_SALARY_WIDGETS = {
+    "hours_per_day": forms.NumberInput(
+        attrs={
+            "class": "form-control",
+            "type": "number",
+            "min": "1",
+            "max": "24",
+        }
+    ),
+    "salary_date": forms.DateInput(attrs={"class": "form-control", "type": "date"}),
+}
+
 
 class DailySalaryForm(forms.ModelForm):
     class Meta:
         model = DailySalary
-        fields = "__all__"
+        fields = ["employee", "hours_per_day", "salary_date"]
         widgets = {
+            **COMMON_DAILY_SALARY_WIDGETS,
             "employee": forms.Select(attrs={"class": "form-control"}),
-            "hours_per_day": forms.NumberInput(
-                attrs={
-                    "class": "form-control",
-                    "type": "number",
-                    "min": "1",
-                    "max": "24",
-                }
-            ),
-            "salary_date": forms.DateInput(
-                attrs={"class": "form-control", "type": "date"}
-            ),
         }
 
 
@@ -28,15 +30,5 @@ class UpdateDailySalaryForm(forms.ModelForm):
         model = DailySalary
         fields = ["hours_per_day", "salary_date"]
         widgets = {
-            "hours_per_day": forms.NumberInput(
-                attrs={
-                    "class": "form-control",
-                    "type": "number",
-                    "min": "1",
-                    "max": "24",
-                }
-            ),
-            "salary_date": forms.DateInput(
-                attrs={"class": "form-control", "type": "date"}
-            ),
+            **COMMON_DAILY_SALARY_WIDGETS,
         }
