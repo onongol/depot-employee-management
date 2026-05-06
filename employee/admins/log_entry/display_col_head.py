30e14f815c5d45cb1761f3211668889ecddd415b
onongol <rozserbatlin@gmail.com>
2026-05-06 11:29:51 +0800

    Fix display function in make_logentry_display to return default value for missing attributes

--- employee/admins/log_entry/display_col_head.py
+++ employee/admins/log_entry/display_col_head.py
@@ -5,7 +5,7 @@ def make_logentry_display(attr_name, title):
     """Factory function to create admin display methods for LogEntry fields."""
 
     @admin.display(description=title)
-    def _display(self, obj):
-        return getattr(obj, attr_name)
+    def _display(_self, obj):
+        return getattr(obj, attr_name, "-")
 
     return _display
