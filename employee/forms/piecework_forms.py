84d62a803132393c447e8bb389a36e7a7e3b0827
onongol <rozserbatlin@gmail.com>
2026-05-06 16:48:33 +0800

    Refactor PieceworkForm and UpdatePieceworkForm to use COMMON_PIECEWORK_WIDGETS for widget definitions

--- employee/forms/piecework_forms.py
+++ employee/forms/piecework_forms.py
@@ -3,21 +3,30 @@ from django import forms
 from employee.forms.forms_mixins.type_work_mixins import TypeWorkChoicesMixin
 from employee.models import Piecework
 
+COMMON_PIECEWORK_WIDGETS = {
+    "type_work": forms.Select(attrs={"class": "form-control"}),
+    "work_date": forms.DateInput(attrs={"class": "form-control", "type": "date"}),
+    "amount": forms.NumberInput(
+        attrs={"class": "form-control", "type": "number", "min": "0.01"}
+    ),
+}
+
 
 class PieceworkForm(TypeWorkChoicesMixin, forms.ModelForm):
     class Meta:
         model = Piecework
-        fields = "__all__"
+        fields = [
+            "employee",
+            "work",
+            "type_work",
+            "wagon_number",
+            "amount",
+            "work_date",
+        ]
         widgets = {
+            **COMMON_PIECEWORK_WIDGETS,
             "employee": forms.Select(attrs={"class": "form-control"}),
             "work": forms.Select(attrs={"class": "form-control"}),
-            "type_work": forms.Select(attrs={"class": "form-control"}),
-            "work_date": forms.DateInput(
-                attrs={"class": "form-control", "type": "date"}
-            ),
-            "amount": forms.NumberInput(
-                attrs={"class": "form-control", "type": "number", "min": "0.01"}
-            ),
         }
 
 
@@ -26,11 +35,5 @@ class UpdatePieceworkForm(TypeWorkChoicesMixin, forms.ModelForm):
         model = Piecework
         fields = ["type_work", "wagon_number", "amount", "work_date"]
         widgets = {
-            "type_work": forms.Select(attrs={"class": "form-control"}),
-            "work_date": forms.DateInput(
-                attrs={"class": "form-control", "type": "date"}
-            ),
-            "amount": forms.NumberInput(
-                attrs={"class": "form-control", "type": "number", "min": "0.01"}
-            ),
+            **COMMON_PIECEWORK_WIDGETS,
         }
