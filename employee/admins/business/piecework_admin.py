9fcb44e3bdae2af2cfb1e5e86b7eb8fa9f7de1e6
onongol <rozserbatlin@gmail.com>
2026-05-06 10:35:29 +0800

    Refactor PieceworkAdmin to use ReadOnlyExportAdminMixin and update search_fields for exact match on employee_code

--- employee/admins/business/piecework_admin.py
+++ employee/admins/business/piecework_admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from import_export.admin import ExportActionModelAdmin, ExportMixin
 from simple_history.admin import SimpleHistoryAdmin
 from unfold.admin import ModelAdmin
 from unfold.contrib.filters.admin import (
@@ -7,20 +6,16 @@ from unfold.contrib.filters.admin import (
     FieldTextFilter,
     MultipleChoicesDropdownFilter,
 )
-from unfold.contrib.import_export.forms import ExportForm
 
-from employee.admins.common import ReadOnlyAdminMixin
+from employee.admins.common import ReadOnlyExportAdminMixin
 from employee.models import Piecework
 
 
 @admin.register(Piecework)
 class PieceworkAdmin(
-    ReadOnlyAdminMixin,
+    ReadOnlyExportAdminMixin,
     ModelAdmin,
     SimpleHistoryAdmin,
-    ExportActionModelAdmin,
-    ExportMixin,
-    ExportForm,
 ):
     list_display = (
         "employee_code",
@@ -39,7 +34,7 @@ class PieceworkAdmin(
         "record_date",
     )
     search_fields = (
-        "employee_code",
+        "employee_code__exact",
         "employee_name",
     )
     search_help_text = "Search by employee ID or name"
@@ -52,7 +47,6 @@ class PieceworkAdmin(
         ("wagon_number", FieldTextFilter),
         ("type_wagon", MultipleChoicesDropdownFilter),
     ]
-    list_select_related = ("employee", "work", "daily_work")
     date_hierarchy = "work_date"
     ordering = ("-work_date", "-record_date")
     list_display_links = ("employee_code", "employee_name", "work_name")
