1c40b7010c8a80394bbc9ff22343780d2d0b1624
onongol <rozserbatlin@gmail.com>
2026-05-06 09:23:51 +0800

    Normalize salary_date in create_daily_salary_instance and extract year/month

--- employee/views/daily_salary/daily_salary_create/daily_salary_create_instance.py
+++ employee/views/daily_salary/daily_salary_create/daily_salary_create_instance.py
@@ -1,7 +1,11 @@
 from employee.models.daily_salary_models import DailySalary
+from employee.utils.converting_date import format_date
 
 
 def create_daily_salary_instance(emp, salary_date, hours_per_day):
+    normalized_salary_date = (
+        format_date(salary_date) if isinstance(salary_date, str) else salary_date
+    )
     salary_day = float(hours_per_day) * float(emp.money_per_hour)
 
     return DailySalary(
@@ -10,7 +14,9 @@ def create_daily_salary_instance(emp, salary_date, hours_per_day):
         employee_name=emp.employee_name,
         department=emp.department,
         job_title=emp.job_title,
-        salary_date=salary_date,
+        salary_date=normalized_salary_date,
+        salary_year=normalized_salary_date.year if normalized_salary_date else None,
+        salary_month=normalized_salary_date.month if normalized_salary_date else None,
         hours_per_day=hours_per_day,
         salary_day=salary_day,
     )
