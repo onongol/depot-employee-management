1da4cca80f3c8aeb3a3d126aba12d0c00092097b
onongol <rozserbatlin@gmail.com>
2026-05-06 16:48:38 +0800

    Refactor WorkForm and UpdateWorkForm to use COMMON_WORK_WIDGETS for widget definitions

--- employee/forms/work_forms.py
+++ employee/forms/work_forms.py
@@ -1,56 +1,58 @@
 from django import forms
 
-from employee.forms.forms_mixins.type_wagon_mixins import TypeWagonChoicesMixin
 from employee.forms.forms_mixins.job_title_mixins import JobTitleChoicesMixin
+from employee.forms.forms_mixins.type_wagon_mixins import TypeWagonChoicesMixin
 from employee.models import Work
 
+COMMON_WORK_WIDGETS = {
+    "job_title": forms.Select(attrs={"class": "form-control"}),
+    "type_wagon": forms.Select(attrs={"class": "form-control"}),
+    "type_material": forms.TextInput(attrs={"class": "form-control"}),
+    "usage_material": forms.NumberInput(
+        attrs={
+            "class": "form-control",
+            "type": "number",
+            "min": "0",
+        }
+    ),
+    "standard_time": forms.NumberInput(
+        attrs={
+            "class": "form-control",
+            "type": "number",
+            "min": "0.000001",
+            "step": "0.000001",
+        }
+    ),
+    "price": forms.NumberInput(
+        attrs={
+            "class": "form-control",
+            "type": "number",
+            "min": "0.01",
+            "step": "0.01",
+        }
+    ),
+}
 
-class WorkForm(
-    TypeWagonChoicesMixin, JobTitleChoicesMixin, forms.ModelForm
-):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
+class WorkForm(TypeWagonChoicesMixin, JobTitleChoicesMixin, forms.ModelForm):
     class Meta:
         model = Work
-        fields = "__all__"
+        fields = [
+            "work_name",
+            "job_title",
+            "type_wagon",
+            "type_material",
+            "usage_material",
+            "standard_time",
+            "price",
+        ]
         widgets = {
-            "job_title": forms.Select(attrs={"class": "form-control"}),
+            **COMMON_WORK_WIDGETS,
             "work_name": forms.TextInput(attrs={"class": "form-control"}),
-            "type_wagon": forms.Select(attrs={"class": "form-control"}),
-            "type_material": forms.TextInput(attrs={"class": "form-control"}),
-            "usage_material": forms.NumberInput(
-                attrs={
-                    "class": "form-control",
-                    "type": "number",
-                    "min": "0",
-                }
-            ),
-            "standard_time": forms.NumberInput(
-                attrs={
-                    "class": "form-control",
-                    "type": "number",
-                    "min": "0.000001",
-                    "step": "0.000001",
-                }
-            ),
-            "price": forms.NumberInput(
-                attrs={
-                    "class": "form-control",
-                    "type": "number",
-                    "min": "0.01",
-                    "step": "0.01",
-                }
-            ),
         }
 
 
-class UpdateWorkForm(
-    TypeWagonChoicesMixin, JobTitleChoicesMixin, forms.ModelForm
-):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
+class UpdateWorkForm(TypeWagonChoicesMixin, JobTitleChoicesMixin, forms.ModelForm):
     class Meta:
         model = Work
         fields = [
@@ -62,30 +64,5 @@ class UpdateWorkForm(
             "price",
         ]
         widgets = {
-            "job_title": forms.Select(attrs={"class": "form-control"}),
-            "type_wagon": forms.Select(attrs={"class": "form-control"}),
-            "type_material": forms.TextInput(attrs={"class": "form-control"}),
-            "usage_material": forms.NumberInput(
-                attrs={
-                    "class": "form-control",
-                    "type": "number",
-                    "min": "0",
-                }
-            ),
-            "standard_time": forms.NumberInput(
-                attrs={
-                    "class": "form-control",
-                    "type": "number",
-                    "min": "0",
-                    "step": "0.000001",
-                }
-            ),
-            "price": forms.NumberInput(
-                attrs={
-                    "class": "form-control",
-                    "type": "number",
-                    "min": "0",
-                    "step": "0.01",
-                }
-            ),
+            **COMMON_WORK_WIDGETS,
         }
