1c6f863d2f7b73c721f472f9aad95ef8c5d4bc53
onongol <rozserbatlin@gmail.com>
2026-05-07 14:08:46 +0800

    Refactor MechanicContextMixin to update docstring and improve context data handling

--- employee/mixins/mechanic_context_mixins.py
+++ employee/mixins/mechanic_context_mixins.py
@@ -3,12 +3,13 @@ from employee.utils.select_department import get_selected_department
 
 
 class MechanicContextMixin:
-    """Add MECHANIC constant to context."""
+    """Add is_mechanic flag to context."""
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        dept = getattr(
-            getattr(self, "object", None), "department", None
-        ) or get_selected_department(self.request)
-        context["MECHANIC"] = dept == MECHANIC
+        obj = getattr(self, "object", None)
+        dept = (obj.department if obj is not None else None) or get_selected_department(
+            self.request
+        )
+        context["is_mechanic"] = dept == MECHANIC
         return context
