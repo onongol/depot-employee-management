f6b46efcfa7af4c2bbf4c00c6e75f9a75d5b9919
onongol <rozserbatlin@gmail.com>
2026-05-07 10:29:03 +0800

    Refactor UserContextCacheMiddleware docstring and reorder method calls for clarity

--- employee/middleware/user_context_cache.py
+++ employee/middleware/user_context_cache.py
@@ -5,16 +5,17 @@ from employee.utils.user_roles import get_user_groups
 
 
 class UserContextCacheMiddleware:
+    """Caches user context (department and groups) for the duration of the request."""
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        """Middleware to cache user context department and groups for the duration of the request."""
-
-        get_selected_department_from_request(request)
 
         if request.user.is_authenticated:
             get_user_groups(request)
+
+        get_selected_department_from_request(request)
 
         response = self.get_response(request)
         return response
