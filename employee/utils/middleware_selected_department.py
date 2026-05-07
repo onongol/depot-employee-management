2706e4f35a08dc96df237d2e1e1e7662d6585db9
onongol <rozserbatlin@gmail.com>
2026-05-07 14:09:46 +0800

    Refactor get_selected_department_from_request to validate selected department against constants

--- employee/utils/middleware_selected_department.py
+++ employee/utils/middleware_selected_department.py
@@ -1,7 +1,14 @@
+from employee.constants.constants import DEPARTMENTS
+
+
 def get_selected_department_from_request(request):
     """
     Get the selected department from request.GET or request.session and set it on request for reuse.
     """
-    selected_dep = request.GET.get("department") or request.session.get("department")
-    request.selected_department = selected_dep
-    return selected_dep
+    selected_department = request.GET.get("department") or request.session.get(
+        "department"
+    )
+    if selected_department not in DEPARTMENTS:
+        selected_department = None
+    request.selected_department = selected_department
+    return selected_department
