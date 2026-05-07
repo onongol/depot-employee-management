6021941dc8b01886184e0ac0c8f9983fa5855f43
onongol <rozserbatlin@gmail.com>
2026-05-07 14:09:39 +0800

    Refactor SoftDeleteMixin to ensure atomic transactions for delete and restore methods

--- employee/models/models_mixins/soft_delete_mixin.py
+++ employee/models/models_mixins/soft_delete_mixin.py
@@ -1,19 +1,24 @@
+from django.db import transaction
 from simple_history.utils import update_change_reason
 
 
 class SoftDeleteMixin:
+    """Mixin providing soft delete, restore, and hard delete for models."""
+
     def delete(self, using=None, keep_parents=False):
         """Soft delete the record by setting is_deleted to True."""
-        self.is_deleted = True
-        self.save(update_fields=["is_deleted"])
-        update_change_reason(self, "Soft deleted")
+        with transaction.atomic():
+            self.is_deleted = True
+            self.save(update_fields=["is_deleted"])
+            update_change_reason(self, "Soft deleted")
 
     def restore(self):
         """Restore a soft-deleted record by setting is_deleted to False."""
-        self.is_deleted = False
-        self.save(update_fields=["is_deleted"])
-        update_change_reason(self, "Restored")
+        with transaction.atomic():
+            self.is_deleted = False
+            self.save(update_fields=["is_deleted"])
+            update_change_reason(self, "Restored")
 
     def hard_delete(self, using=None, keep_parents=False):
-        """Permanently delete the record from the database."""
+        """Permanently delete the record. History entry is not created."""
         super().delete(using=using, keep_parents=keep_parents)
