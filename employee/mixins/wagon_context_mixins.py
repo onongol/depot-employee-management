55416ac63c628e061e2761f3f0fabddf57a2dd7c
onongol <rozserbatlin@gmail.com>
2026-05-07 14:09:11 +0800

    Refactor WagonContextMixin to update flag handling and improve context data retrieval

--- employee/mixins/wagon_context_mixins.py
+++ employee/mixins/wagon_context_mixins.py
@@ -1,14 +1,15 @@
-from employee.constants.constants import ALLOWED_WAGON_DEPARTMENTS
 from employee.utils.select_department import get_selected_department
+from employee.utils.wagon_department import is_wagon_department
 
 
 class WagonContextMixin:
-    """Add SHOW_TYPE_WAGON flag based on department."""
+    """Add is_wagon_department flag based on department."""
 
     def get_context_data(self, **kwargs):
-        ctx = super().get_context_data(**kwargs)
-        dept = getattr(
-            getattr(self, "object", None), "department", None
-        ) or get_selected_department(self.request)
-        ctx["SHOW_TYPE_WAGON"] = dept in ALLOWED_WAGON_DEPARTMENTS
-        return ctx
+        context = super().get_context_data(**kwargs)
+        obj = getattr(self, "object", None)
+        dept = (obj.department if obj is not None else None) or get_selected_department(
+            self.request
+        )
+        context["is_wagon_department"] = is_wagon_department(dept)
+        return context
