b5084d6208a03e55eaa2c2c1142d0949bd7dc567
onongol <rozserbatlin@gmail.com>
2026-05-07 08:46:43 +0800

    Refactor send_delete_success_message to correct docstring and optimize translation import

--- employee/messages/delete_success_message.py
+++ employee/messages/delete_success_message.py
@@ -1,13 +1,10 @@
 from django.contrib import messages
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 
 
 def send_delete_success_message(request, object_name):
-    """Send a warning message after deleting an object."""
+    """Send a success message after deleting an object."""
     messages.success(
         request,
-        _(
-            "Deleted %(object_name)s"
-        )
-        % {"object_name": object_name},
+        _("Deleted %(object_name)s") % {"object_name": object_name},
     )
