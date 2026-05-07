53e08c71c8d35d9954957316889f2be5e4bad264
onongol <rozserbatlin@gmail.com>
2026-05-07 08:47:36 +0800

    Refactor delete_bulk_message to use gettext for translations and set default values for parameters

--- employee/messages/bulk_delete_messages/delete_bulk_message.py
+++ employee/messages/bulk_delete_messages/delete_bulk_message.py
@@ -1,14 +1,16 @@
 from django.contrib import messages
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 
 
 def delete_bulk_message(
     request,
     deleted_count,
-    deletable_items,
-    deletable_tail,
+    deletable_items=None,
+    deletable_tail="",
 ):
     """Displays a success message after bulk deletion, showing a preview of deleted items and a summary tail for user feedback."""
+    deletable_items = deletable_items or []
+
     if deleted_count:
         messages.success(
             request,
