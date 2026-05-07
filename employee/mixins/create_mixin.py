d1c6c6eae2b75edcc4d1294fd3cf4afc0397449d
onongol <rozserbatlin@gmail.com>
2026-05-07 14:08:29 +0800

    Fix docstring formatting in AdminLoggedCreateMixin for clarity

--- employee/mixins/create_mixin.py
+++ employee/mixins/create_mixin.py
@@ -4,6 +4,8 @@ from employee.services.admin_log_entries import log_object_addition
 
 
 class AdminLoggedCreateMixin:
+    """Mixin to log object creation in the admin interface."""
+
     def form_valid(self, form):
         with transaction.atomic():
             response = super().form_valid(form)
