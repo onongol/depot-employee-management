1b2ae44cb50dcdaf6bb63367d909df0fe5e7d813
onongol <rozserbatlin@gmail.com>
2026-05-08 14:55:47 +0800

    Refactor apply_ordering function to improve docstring clarity and remove unnecessary parameter descriptions

--- employee/utils/sorting.py
+++ employee/utils/sorting.py
@@ -1,15 +1,7 @@
 def apply_ordering(
     queryset, order_by, direction, allowed_fields, default="-record_date"
 ):
-    """
-    Applies ordering to a queryset based on the specified field and direction.
-    :param queryset: QuerySet to be ordered
-    :param order_by: field name to order by (str)
-    :param direction: 'asc' or 'desc'
-    :param allowed_fields: list of allowed fields for ordering
-    :param default: default ordering field or list/tuple of fields
-    :return: ordered QuerySet
-    """
+    """Applies ordering to a queryset based on the specified field and direction."""
     if order_by in allowed_fields:
         sort_field = f"-{order_by}" if direction == "desc" else order_by
         return queryset.order_by(sort_field)
