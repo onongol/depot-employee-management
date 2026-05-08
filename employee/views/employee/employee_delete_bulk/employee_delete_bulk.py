27fbb3e9fb50cbf0cffd58b1e9fa3237c0c0e51b
onongol <rozserbatlin@gmail.com>
2026-05-08 11:44:56 +0800

    Change user access test from is_admin to is_payroll for bulk employee deletion

--- employee/views/employee/employee_delete_bulk/employee_delete_bulk.py
+++ employee/views/employee/employee_delete_bulk/employee_delete_bulk.py
@@ -15,7 +15,7 @@ from employee.messages.bulk_delete_messages.delete_bulk_message import (
     delete_bulk_message,
 )
 from employee.services.admin_log_delete import delete_queryset_with_admin_log
-from employee.utils.access import is_admin
+from employee.utils.access import is_payroll
 from employee.utils.parse_ids import parse_ids
 from employee.utils.select_department import get_selected_department
 from employee.views.employee.employee_delete_bulk.selectors import (
@@ -25,7 +25,7 @@ from employee.views.employee.employee_delete_bulk.selectors import (
 
 @require_POST
 @login_required
-@user_passes_test(is_admin)
+@user_passes_test(is_payroll)
 def employee_delete_bulk(request):
     department = get_selected_department(request)
     fallback_url = reverse("employee_list")
