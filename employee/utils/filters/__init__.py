9c7435e51f4f610c9f7fad47005567c1023a1d57
onongol <rozserbatlin@gmail.com>
2026-08-24 10:09:52 +0800

    Remove unused filter_employees_salary import from filters

--- employee/utils/filters/__init__.py
+++ employee/utils/filters/__init__.py
@@ -1,7 +1,6 @@
 from .filter_daily_salaries import filter_daily_salaries
 from .filter_daily_works import filter_daily_works
 from .filter_employees import filter_employees
-from .filter_employees_salary import filter_employees_salary
 from .filter_material import filter_material
 from .filter_month_year import filter_month_year
 from .filter_pieceworks import filter_pieceworks
@@ -12,7 +11,6 @@ __all__ = [
     "filter_daily_salaries",
     "filter_daily_works",
     "filter_employees",
-    "filter_employees_salary",
     "filter_material",
     "filter_month_year",
     "filter_pieceworks",
